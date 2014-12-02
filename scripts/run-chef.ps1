b50d8cacf6d9dc74292a8221eb4881ae93289800
Rob Goodridge <rob.goodridge@lansa.com.au>
2014-12-02 15:46:45 +1100

    Windows Stack deploying a working MSI. May update Stack to: apply windows updates, apply chef updates, install MSI Upgrade, install LANSA MSP patches

---
+++ scripts/run-chef.ps1
@@ -0,0 +1,49 @@
+﻿<#
+.SYNOPSIS
+
+Run Chef so that we can run as administrator. Cannot directly run from the Cloud Formation script
+
+#>
+param(
+[String]$FullPath='c:\opscode\chef\bin\chef-client.bat',
+[String]$WorkingDirectory='C:\recipes\chef-repo\cookbooks',
+[String]$cookbook='VLWebServer',
+[String]$Arguments="-z -o $cookbook"
+)
+
+# Put first output on a new line in cfn_init log file
+Write-Output ("`r`n")
+
+# $DebugPreference = "Continue"
+
+Write-Debug ("Cookbook = $cookbook")
+
+$ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo 
+$ProcessInfo.FileName = $FullPath 
+$ProcessInfo.WorkingDirectory = $WorkingDirectory
+$ProcessInfo.RedirectStandardError = $true 
+$ProcessInfo.RedirectStandardOutput = $true 
+$ProcessInfo.UseShellExecute = $false 
+$ProcessInfo.Arguments = $Arguments 
+$Process = New-Object System.Diagnostics.Process 
+$Process.StartInfo = $ProcessInfo 
+$Process.Start() | Out-Null 
+$std_output = $Process.StandardOutput.ReadToEnd() 
+$std_error = $Process.StandardError.ReadToEnd() 
+$Process.WaitForExit() 
+Write-Output "Standard Output..."
+$std_output 
+if ( $std_error) {
+    Write-Output "Standard Error..."
+    $std_error
+}
+
+if ( $Process.ExitCode -eq 0 )
+{
+    Write-Output ( "Installation successful")
+}
+else
+{
+    Write-Output ( "Installation failed")
+}
+exit $Process.ExitCode
