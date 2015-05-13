c33ec28120592c80f6e52f54f757ded88a4b32c1
Rob Goodridge <rob.goodridge@lansa.com.au>
2015-05-13 17:31:50 +1000

    Get more privileges before trying to log everyone off

--- scripts/win-updates.ps1
+++ scripts/win-updates.ps1
@@ -239,10 +239,12 @@ function Logoff-Allusers()
 	# Force Logoff all RDP users so that reboot will work and applying updates less likely to fail 
 	LogWrite 'Forcing logoff of all users'
 	
-	(gwmi win32_operatingsystem -ComputerName $env:COMPUTERNAME).Win32Shutdown(4)
+	$win32OS = get-wmiobject win32_operatingsystem -computername $ENV:COMPUTERNAME
+	$win32OS.psbase.Scope.Options.EnablePrivileges = $true
+	$win32OS.win32shutdown(4)
 
-	LogWrite
 	# Wait to make sure the users have actually been logged off
+	# Not sure if this makes a difference
 	Start-Sleep -s 10
 
 	LogWrite 'Users have been logged off'
