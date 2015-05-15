1bd298278d018e41c6efaffe4124196992568d6e
Rob Goodridge <rob.goodridge@lansa.com.au>
2015-05-15 16:30:52 +1000

    Preliminary windows updates reboot detection changes

--- scripts/win-updates.ps1
+++ scripts/win-updates.ps1
@@ -15,56 +15,47 @@ function LogWrite {
 function Check-ContinueRestartOrEnd() {
     $RegistryKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
     $RegistryEntry = "InstallWindowsUpdates"
-	try
-	{
-		switch ($global:RestartRequired) {
-			0 {
-				$prop = (Get-ItemProperty $RegistryKey).$RegistryEntry
-				if ($prop) {
-					LogWrite "Restart Registry Entry Exists - Removing It"
-					Remove-ItemProperty -Path $RegistryKey -Name $RegistryEntry -ErrorAction SilentlyContinue
-				}
-
-				LogWrite "No Restart Required"
-				Check-WindowsUpdates
-
-				if (($global:MoreUpdates -eq 1) -and ($script:Cycles -le $global:MaxCycles)) {
-					Install-WindowsUpdates
-				} elseif ($script:Cycles -gt $global:MaxCycles) {
-					LogWrite "Exceeded Cycle Count - Stopping"
-				} else {
-					LogWrite "Done Installing Windows Updates"
-				}
+	switch ($global:RestartRequired) {
+		0 {
+			$prop = (Get-ItemProperty $RegistryKey).$RegistryEntry
+			if ($prop) {
+				LogWrite "Restart Registry Entry Exists - Removing It"
+				Remove-ItemProperty -Path $RegistryKey -Name $RegistryEntry -ErrorAction SilentlyContinue
 			}
-			1 {
-	# AU - We don't want to run Windows update again. It seems "Run" doesn't automatically run after a login.
-	# We saw evidence of it running 20 minutes after a login and that wasn't even the first login after the
-	# instance was created.
-	<#
-				$prop = (Get-ItemProperty $RegistryKey).$RegistryEntry
-				if (-not $prop) {
-					LogWrite "Restart Registry Entry Does Not Exist - Creating It"
-					Set-ItemProperty -Path $RegistryKey -Name $RegistryEntry -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File $($script:ScriptPath) -MaxUpdatesPerCycle $($MaxUpdatesPerCycle)"
-				} else {
-					LogWrite "Restart Registry Entry Exists Already"
-				}
-	#>
-				Logoff-Allusers
 
-				LogWrite "Restart Required - Restarting..."
-				Restart-Computer -force
-			}
-			default {
-				LogWrite "Unsure If A Restart Is Required"
-				break
+			LogWrite "No Restart Required"
+			Check-WindowsUpdates
+
+			if (($global:MoreUpdates -eq 1) -and ($script:Cycles -le $global:MaxCycles)) {
+				Install-WindowsUpdates
+			} elseif ($script:Cycles -gt $global:MaxCycles) {
+				LogWrite "Exceeded Cycle Count - Stopping"
+			} else {
+				LogWrite "Done Installing Windows Updates"
 			}
 		}
-	}
-	catch
-	{
-		LogWrite( $_ )
-		LogWrite("Installation error: reboot failed?")
-		exit 2
+		1 {
+# AU - We don't want to run Windows update again. It seems "Run" doesn't automatically run after a login.
+# We saw evidence of it running 20 minutes after a login and that wasn't even the first login after the
+# instance was created.
+<#
+			$prop = (Get-ItemProperty $RegistryKey).$RegistryEntry
+			if (-not $prop) {
+				LogWrite "Restart Registry Entry Does Not Exist - Creating It"
+				Set-ItemProperty -Path $RegistryKey -Name $RegistryEntry -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File $($script:ScriptPath) -MaxUpdatesPerCycle $($MaxUpdatesPerCycle)"
+			} else {
+				LogWrite "Restart Registry Entry Exists Already"
+			}
+#>
+			Logoff-Allusers
+
+			LogWrite "Restart Required - Restarting..."
+			Restart-Computer -force
+		}
+		default {
+			LogWrite "Unsure If A Restart Is Required"
+			break
+		}
 	}
 }
 
@@ -176,62 +167,54 @@ function Check-WindowsUpdates() {
     LogWrite "Checking For Windows Updates"
     $Username = $env:USERDOMAIN + "\" + $env:USERNAME
 
-	try
-	{
-		New-EventLog -Source $ScriptName -LogName 'Windows Powershell' -ErrorAction SilentlyContinue
+	New-EventLog -Source $ScriptName -LogName 'Windows Powershell' -ErrorAction SilentlyContinue
 
-		$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
 
-		Write-EventLog -LogName 'Windows Powershell' -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message
+	Write-EventLog -LogName 'Windows Powershell' -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message
+	LogWrite $Message
+
+	$script:UpdateSearcher = $script:UpdateSession.CreateUpdateSearcher()
+	$script:successful = $FALSE
+	$script:attempts = 0
+	$script:maxAttempts = 12
+	while(-not $script:successful -and $script:attempts -lt $script:maxAttempts) {
+		try {
+			$script:SearchResult = $script:UpdateSearcher.Search("IsInstalled=0 and Type='Software' and IsHidden=0")
+			$script:successful = $TRUE
+		} catch {
+			LogWrite $_.Exception | Format-List -force
+			LogWrite "Search call to UpdateSearcher was unsuccessful. Retrying in 10s."
+			$script:attempts = $script:attempts + 1
+			Start-Sleep -s 10
+		}
+	}
+
+	if ($SearchResult.Updates.Count -ne 0) {
+		$Message = "There are " + $SearchResult.Updates.Count + " more updates."
 		LogWrite $Message
-
-		$script:UpdateSearcher = $script:UpdateSession.CreateUpdateSearcher()
-		$script:successful = $FALSE
-		$script:attempts = 0
-		$script:maxAttempts = 12
-		while(-not $script:successful -and $script:attempts -lt $script:maxAttempts) {
-			try {
-				$script:SearchResult = $script:UpdateSearcher.Search("IsInstalled=0 and Type='Software' and IsHidden=0")
-				$script:successful = $TRUE
-			} catch {
-				LogWrite $_.Exception | Format-List -force
-				LogWrite "Search call to UpdateSearcher was unsuccessful. Retrying in 10s."
-				$script:attempts = $script:attempts + 1
-				Start-Sleep -s 10
-			}
-		}
-
-		if ($SearchResult.Updates.Count -ne 0) {
-			$Message = "There are " + $SearchResult.Updates.Count + " more updates."
-			LogWrite $Message
-			try {
-				$script:SearchResult.Updates |Select-Object -Property Title, Description, SupportUrl, UninstallationNotes, RebootRequired, EulaAccepted |Format-List
-				$global:MoreUpdates=1
-			} catch {
-				LogWrite $_.Exception | Format-List -force
-				LogWrite "Showing SearchResult was unsuccessful. Rebooting."
-				$global:RestartRequired=1
-				$global:MoreUpdates=0
-				Check-ContinueRestartOrEnd
-				LogWrite "Should never see this text! Reboot should have already occurred"
-
-				Logoff-Allusers
-
-				LogWrite "Restart Required - Restarting..."
-				Restart-Computer -Force
-			}
-		} else {
-			LogWrite 'There are no applicable updates'
-			$global:RestartRequired=0
+		try {
+			$script:SearchResult.Updates |Select-Object -Property Title, Description, SupportUrl, UninstallationNotes, RebootRequired, EulaAccepted |Format-List
+			$global:MoreUpdates=1
+		} catch {
+			LogWrite $_.Exception | Format-List -force
+			LogWrite "Showing SearchResult was unsuccessful. Rebooting."
+			$global:RestartRequired=1
 			$global:MoreUpdates=0
+			Check-ContinueRestartOrEnd
+			LogWrite "Should never see this text! Reboot should have already occurred"
+
+			Logoff-Allusers
+
+			LogWrite "Restart Required - Restarting..."
+			Restart-Computer -Force
 		}
+	} else {
+		LogWrite 'There are no applicable updates'
+		$global:RestartRequired=0
+		$global:MoreUpdates=0
 	}
-	catch
-	{
-		LogWrite( $_ )
-		LogWrite("Installation error: reboot failed?")
-		exit 2
-	}
+
 }
 
 function Logoff-Allusers()
@@ -250,6 +233,27 @@ function Logoff-Allusers()
 	LogWrite 'Users have been logged off'
 }
 
+# Check registry for restarting flags
+function CheckRestart([REF]$retval)
+{
+	$cn = $ENV:COMPUTERNAME
+    [bool]$PendingFile = $false
+    [bool]$AutoUpdate = $false
+
+	#Determine PendingFileRenameOperations exists or not  
+	$PendFileKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\" 
+	Invoke-Command -ComputerName $cn -ErrorAction SilentlyContinue -ScriptBlock{ 
+	Get-ItemProperty -Path $using:PendFileKeyPath -name PendingFileRenameOperations} |` 
+	Foreach{If($_.PendingFileRenameOperations){$PendingFile = $true}Else{$PendingFile = $false}} 
+  
+	#Determine RebootRequired subkey exists or not 
+	$AutoUpdateKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" 
+	Invoke-Command -ComputerName $cn -ErrorAction SilentlyContinue -ScriptBlock {Test-Path -Path "$using:AutoUpdateKeyPath\RebootRequired"} |` 
+	Foreach{If($_ -eq $true){$AutoUpdate = $true}Else{$AutoUpdate = $false}} 
+
+	$retval.Value = ($AutoUpdate -or $PendingFile)
+}
+
 $script:ScriptName = $MyInvocation.MyCommand.ToString()
 $script:ScriptPath = $MyInvocation.MyCommand.Path
 $script:UpdateSession = New-Object -ComObject 'Microsoft.Update.Session'
@@ -259,11 +263,34 @@ $script:SearchResult = New-Object -ComObject 'Microsoft.Update.UpdateColl'
 $script:Cycles = 0
 $script:CycleUpdateCount = 0
 
-Logoff-Allusers
+try
+{
+	Logoff-Allusers
 
-Check-WindowsUpdates
-if ($global:MoreUpdates -eq 1) {
-    Install-WindowsUpdates
-} else {
-    Check-ContinueRestartOrEnd
+	Check-WindowsUpdates
+	if ($global:MoreUpdates -eq 1) {
+		Install-WindowsUpdates
+	} else {
+		Check-ContinueRestartOrEnd
+	}
+}
+catch
+{
+	$_
+	LogWrite( $_ )
+	LogWrite("Installation error: logoff failed? reboot failed?")
+	exit 2
+}
+
+
+# Check for restart in case reboot not detected or a prior need for reboot has failed
+# e.g. due to logged on users.
+[bool]$restart = $false
+CheckRestart( [REF]$restart)
+if ( $restart )
+{
+	Logoff-Allusers
+
+	LogWrite "Restart Required - Restarting..."
+	Restart-Computer -Force
 }
