7c41b6fdbedd4304531512a0a7741f809853d398
Rob Goodridge <rob.goodridge@lansa.com.au>
2015-06-03 20:50:20 +1000

    150079 logging off users abended if there were no users logged on.

--- scripts/dot-logoff-allusers.ps1
+++ scripts/dot-logoff-allusers.ps1
@@ -1,15 +1,48 @@
 function Logoff-Allusers()
 {
-	# Force Logoff all RDP users so that reboot will work and applying updates less likely to fail 
-	LogWrite 'Forcing logoff of all users'
+    # Check if more than the current user is logged on
+    $Computer = $ENV:COMPUTERNAME
+    # Redirect stderr to a file otherwise the script displays an error which SilentlyContinue and capturing the exception do not trap.
+    [object[]]$sessions = Invoke-Expression ".\PsLoggedon.exe /accepteula -x 2>c:\lansa\temp.txt " |
+        Where-Object {$_ -match '^\s{2,}(((?<domain>.*)\\(?<user>\S+))|(?<user>\S+))'} |
+        Select-Object @{
+            Name='Computer'
+            Expression={$Computer}
+        },
+        @{
+            Name='Domain'
+            Expression={$matches.Domain}
+        },
+        @{
+            Name='User'
+            Expression={$Matches.User}
+        }
+
+    # Check for >= 2 so we don't logoff the user running the script if they are the only one.
+    IF ($Sessions.count -ge 2)
+    {
+	    # Force Logoff all RDP users so that reboot will work and applying updates less likely to fail 
+	    LogWrite 'Forcing logoff of all users'
 	
-	$win32OS = get-wmiobject win32_operatingsystem -computername $ENV:COMPUTERNAME
-	$win32OS.psbase.Scope.Options.EnablePrivileges = $true
-	$win32OS.win32shutdown(4)
+        try
+        {
+	        $win32OS = get-wmiobject win32_operatingsystem -computername $Computer
+	        $win32OS.psbase.Scope.Options.EnablePrivileges = $true
+	        $win32OS.win32shutdown(4)
 
-	# Wait to make sure the users have actually been logged off
-	# Not sure if this makes a difference
-	Start-Sleep -s 10
+	        # Wait to make sure the users have actually been logged off
+	        # Not sure if this makes a difference
+	        Start-Sleep -s 10
 
-	LogWrite 'Users have been logged off'
+	        LogWrite 'Users have been logged off'
+        }
+        catch
+        {
+            LogWrite 'Attempt to log all users off failed. Presumed to be because no users are logged on'
+        }
+    }
+    else
+    {
+        LogWrite '1 or less users logged on'
+    }
 }
