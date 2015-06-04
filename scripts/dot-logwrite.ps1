d98ee264b5db0ef67707d545e000b61adbd5c053
Rob Goodridge <rob.goodridge@lansa.com.au>
2015-06-04 11:15:37 +1000

    150079 typo

--- scripts/dot-logwrite.ps1
+++ scripts/dot-logwrite.ps1
@@ -3,6 +3,6 @@ $Script:Logfile = "C:\Windows\Temp\win-updates.log"
 function LogWrite {
    Param ([string]$logstring)
    $now = Get-Date -format s
-   Add-Content $Scipt:Logfile -value "$now $logstring"
+   Add-Content $Script:Logfile -value "$now $logstring"
    Write-Output $logstring
 }
