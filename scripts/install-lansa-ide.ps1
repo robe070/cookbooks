104aae9e1ae6e29f2135ccdd91b7f5fedbeb1359
Rob Goodridge <rob.goodridge@lansa.com.au>
2015-08-14 10:59:24 +1000

    153929 Bake IDE

--- scripts/install-lansa-ide.ps1
+++ scripts/install-lansa-ide.ps1
@@ -28,6 +28,21 @@ param(
 [String]$wait
 )
 
+# If environment not yet set up, it should be running locally, not through Remote PS
+if ( -not $script:IncludeDir)
+{
+	Write-Output "Initialising environment - presumed not running through RemotePS"
+	$MyInvocation.MyCommand.Path
+	$script:IncludeDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+	. "$script:IncludeDir\Init-Baking-Vars.ps1"
+	. "$script:IncludeDir\Init-Baking-Includes.ps1"
+}
+else
+{
+	Write-Output "Environment already initialised - presumed running through RemotePS"
+}
+
 # Put first output on a new line in cfn_init log file
 Write-Output ("`r`n")
 
