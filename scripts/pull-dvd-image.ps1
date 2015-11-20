da4ee6556b897f3435ed61e38f89b688e4691b29
Rob Goodridge <rob.goodridge@lansa.com.au>
2015-11-20 11:30:10 +1100

    154747 Create base images for testing

---
+++ scripts/pull-dvd-image.ps1
@@ -0,0 +1,21 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $S3DVDImageDirectory,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $DvdDir,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $SQLServerInstalled
+)
+
+Write-Output "$(Log-Date) S3DVDImageDirectory = $S3DVDImageDirectory, DvdDir = $DvdDir"
+
+if ( $SQLServerInstalled -eq $false) {
+    cmd /c aws s3 sync  $S3DVDImageDirectory $DvdDir "--exclude" "*ibmi/*" "--exclude" "*AS400/*" "--exclude" "*linux/*" "--delete" | Write-Output
+} else {
+    cmd /c aws s3 sync  $S3DVDImageDirectory $DvdDir "--exclude" "*ibmi/*" "--exclude" "*AS400/*" "--exclude" "*linux/*" "--exclude" "*setup/Installs/MSSQLEXP/*" "--delete" | Write-Output
+}
