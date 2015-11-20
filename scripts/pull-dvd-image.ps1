83f51fb775a487e57b8c1bd9e28088389a201aa7
Rob Goodridge <rob.goodridge@lansa.com.au>
2015-11-20 11:36:29 +1100

    154747 Base images for testing

--- scripts/pull-dvd-image.ps1
+++ scripts/pull-dvd-image.ps1
@@ -12,6 +12,22 @@
     $SQLServerInstalled
 )
 
+if ( -not $script:IncludeDir)
+{
+    # Log-Date can't be used yet as Framework has not been loaded
+
+	Write-Output "Initialising environment - presumed not running through RemotePS"
+	$MyInvocation.MyCommand.Path
+	$script:IncludeDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+	. "$script:IncludeDir\Init-Baking-Vars.ps1"
+	. "$script:IncludeDir\Init-Baking-Includes.ps1"
+}
+else
+{
+	Write-Output "$(Log-Date) Environment already initialised - presumed running through RemotePS"
+}
+
 Write-Output "$(Log-Date) S3DVDImageDirectory = $S3DVDImageDirectory, DvdDir = $DvdDir"
 
 if ( $SQLServerInstalled -eq $false) {
