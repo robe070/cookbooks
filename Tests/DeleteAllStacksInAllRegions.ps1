2d6c933b6add5f784936c217aa479a7686bd305f
Rob Goodridge <rob.goodridge@lansa.com.au>
2016-07-21 15:18:36 +1000

    155746 Ensure 132901 stack can be successfully created in all AWS regions

---
+++ Tests/DeleteAllStacksInAllRegions.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.SYNOPSIS
+
+List all stacks in all regions
+
+.EXAMPLE
+
+
+#>
+$regionlist = Get-AWSRegion
+ForEach ( $region in $regionList )
+{
+    Write-Output "$region"
+
+    $nextToken = $null
+    do {
+      [Amazon.CloudFormation.Model.StackSummary[]]$stacks = Get-CFNStackSummary -Region $region -StackStatusFilter @("CREATE_FAILED", "CREATE_IN_PROGRESS", "UPDATE_IN_PROGRESS", "CREATE_COMPLETE"  ) -NextToken $nextToken
+      foreach ( $stack in $stacks )
+      {
+        Write-Output "$($stack.stackName) - $($stack.StackStatus)"
+        Remove-CFNStack -Region $region -StackName $stack.StackName -Force
+      }
+
+      $nextToken = $AWSHistory.LastServiceResponse.NextToken
+    } while ($nextToken -ne $null)
+}
