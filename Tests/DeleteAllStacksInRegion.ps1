f37bb0402d873c06fc869530e3c98760db71a862
Rob Goodridge <rob.goodridge@lansa.com.au>
2017-06-08 16:17:15 +1000

    158137 Enhance test scripts

--- Tests/DeleteAllStacksInRegion.ps1
+++ Tests/DeleteAllStacksInRegion.ps1
@@ -12,7 +12,7 @@ param(
 )
 $nextToken = $null
 do {
-  [Amazon.CloudFormation.Model.StackSummary[]]$stack = Get-CFNStackSummary -Region $region -StackStatusFilter @("CREATE_FAILED", "CREATE_IN_PROGRESS", "UPDATE_IN_PROGRESS", "CREATE_COMPLETE"  ) -NextToken $nextToken
+  [Amazon.CloudFormation.Model.StackSummary[]]$stack = Get-CFNStackSummary -Region $region -StackStatusFilter @("CREATE_FAILED", "DELETE_FAILED", "CREATE_IN_PROGRESS", "UPDATE_IN_PROGRESS", "DELETE_IN_PROGRESS", "CREATE_COMPLETE", "ROLLBACK_IN_PROGRESS", "ROLLBACK_FAILED", "ROLLBACK_COMPLETE", "UPDATE_COMPLETE", "UPDATE_COMPLETE_CLEANUP_IN_PROGRESS", "UPDATE_IN_PROGRESS", "UPDATE_ROLLBACK_COMPLETE", "UPDATE_ROLLBACK_IN_PROGRESS", "UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS", "UPDATE_ROLLBACK_FAILED", "UPDATE_ROLLBACK_IN_PROGRESS", "REVIEW_IN_PROGRESS" ) -NextToken $nextToken
   foreach ( $stackName in $stack.StackName )
   {
     Write-Output "Stack Name = $stackName"
