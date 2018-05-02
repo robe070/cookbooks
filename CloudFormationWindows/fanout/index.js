bd4b4d7db15ab668191c2208fc22cd0459fac6e5
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-05-02 11:57:35 +1000

    159678 Fix up succeed so that it occurs when ALL the EC2 instances have successfully deployed.
    Upgraded to node.js 8.10 and changed to use callback return codes.
    Also added start of querying Route 53 for ELB name.

--- CloudFormationWindows/fanout/index.js
+++ CloudFormationWindows/fanout/index.js
@@ -4,29 +4,13 @@ console.log('Loading function');
 
 var AWS = require('aws-sdk');
 
-//const http = require('http');
-//const keepAliveAgent = new http.Agent({ keepAlive: true });
-//options.agent = keepAliveAgent;
-//http.request(options, onResponseCallback);
-
-function error(e){
-  console.log('--Error--', this.type);
-  console.log('this: ', this);
-  console.log('Event: ', e);
-}
-
 // The code outside of the handler is executed just the once. Make sure it doesn't need to be dynamic.
 
 // AWS.config.update({region: 'us-west-2'});
     
 exports.handler = (event, context, callback) => {
-    
-    function success(s){
-        console.log( 'Successful');
-        callback(null, 'Successfully deployed by lambda');
-    }
+    context.callbackWaitsForEmptyEventLoop = false; 
 
-    var autoscaling = new AWS.AutoScaling();
     var elb = new AWS.ELB();
     var ec2 = new AWS.EC2();
     var http = require('http');
@@ -38,96 +22,137 @@ exports.handler = (event, context, callback) => {
     //console.log('context:', context);
     const message = event.Records[0].Sns.Message;
     const type = event.Records[0].Sns.Type;
-    console.log('Type:', type);
-    console.log('Message:', message.substring(0,100));
+    console.log('SNS Type:', type);
+    //console.log('Message:', message.substring(0,100));
     
     var payload = JSON.parse(message); // converts it to a JS native object.
     // console.log('GitHub Payload:', JSON.stringify(message));
-    console.log('Compare', JSON.stringify( payload.compare) );
+    console.log('GitHub Compare', JSON.stringify( payload.compare) );
 
-    var params = {
-      LoadBalancerNames: [
-        'paas-WebServerELB-W84PXG3L9PEE'
-      ]
+    var route53 = new AWS.Route53();
+    
+    var paramsListRRS = {
+      HostedZoneId: 'Z2K4W96HUY1FNC', /* required - paas.lansa.com */
+      MaxItems: '1',
+      StartRecordName: 'eval1.paas.lansa.com.',
+      StartRecordType: 'A'
     };
+
+    var successCodes = 0;
     
     // Async call
-    elb.describeLoadBalancers(params, function(err, data) {
+    route53.listResourceRecordSets(paramsListRRS, function(err, data) {
         if (err) {
             console.log(err, err.stack); // an error occurred
-            callback(err, err.stack); 
+            callback( err, err.stack );
         } else {
-            // successful response
-
-            var instances = data.LoadBalancerDescriptions[0].Instances;
-            console.log('Instances: ', JSON.stringify(instances).substring(0,400));   
-
-            // forEach is a Sync call
-            Object.keys(instances).forEach(function(key) {
-                console.log("InstanceId[" + key + "] " + JSON.stringify( instances[key].InstanceId ) );
-                
-                var params = {
-                    DryRun: false,
-                    InstanceIds: [
-                        instances[key].InstanceId
-                    ]
-                };
-                
-                // Async call
-                ec2.describeInstances(params, function(err, data) {
-                    if (err) console.log(err, err.stack); // an error occurred
-                    else {
-                        // successful response
-                        // console.log(data.Reservations[0].Instances[0]);        
-                        var PublicIpAddress = data.Reservations[0].Instances[0].PublicIpAddress;
-                        // console.log("Host: ", JSON.stringify( PublicIpAddress ) );
-
-                        // post the payload from GitHub
-                        var post_data = JSON.stringify(message);
-
-                        // console.log("post_data length: ", JSON.stringify( post_data.length ) );
+            console.log('Searched for Alias: ', paramsListRRS.StartRecordName);
+            console.log('Located Alias:      ', data.ResourceRecordSets[0].Name);
+            if ( paramsListRRS.StartRecordName !== data.ResourceRecordSets[0].Name) {
+                var error = new Error('Searched for Alias is not the one located');
+                callback(error);                                         
+            }
+            
+            console.log('ELB DNS Name: ', data.ResourceRecordSets[0].AliasTarget.DNSName);
+            
+            var params = {
+              LoadBalancerNames: [
+                'paas-WebServerELB-W84PXG3L9PEE'
+              ]
+            };
+            
+            // Async call
+            elb.describeLoadBalancers(params, function(err, data) {
+                if (err) {
+                    console.log(err, err.stack); // an error occurred
+                    callback(err, err.stack); 
+                } else {
+                    // successful response
+        
+                    var instances = data.LoadBalancerDescriptions[0].Instances;
+                    console.log('Instances: ', JSON.stringify(instances).substring(0,400));   
+        
+                    var instanceCount = 0;
+                    
+                    // forEach is a Sync call
+                    Object.keys(instances).forEach(function(key) {
+                        console.log("InstanceId[" + key + "] " + JSON.stringify( instances[key].InstanceId ) );
                         
-                        // An object of options to indicate where to post to
-                        var post_options = {
-                            host: PublicIpAddress,
-                            port: '8101',
-                            path: '/Deployment/Start/APP2?source=sourceName',
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Content-Length': post_data.length
-                            }
+                        instanceCount++;
+                        
+                        var params = {
+                            DryRun: false,
+                            InstanceIds: [
+                                instances[key].InstanceId
+                            ]
                         };
-                    
+                        
                         // Async call
-                        var post_request = http.request(post_options, function(res) {
-                            var body = '';
-                    
-                            if (res.statusCode === 200) {
-                                context.succeed('Application update successfully deployed by Lambda function to ' + post_options.path);
+                        ec2.describeInstances(params, function(err, data) {
+                            if (err) {
+                                console.log(err, err.stack); // an error occurred
+                                callback( err, err.stack );
                             } else {
-                                context.fail('status code: ' + res.statusCode);
-                            }                        
-                            res.on('data', function(chunk)  {
-                                body += chunk;
-                            });
-                    
-                            res.on('end', function() {
-                                context.done(body);
-                            });
-                    
-                            res.on('error', function(e) {
-                                context.fail('error posting to EC2 instance:' + e.message);
-                            });
-                        });    
-                        // post the data
-                        console.log( 'Posting to:', post_options.host, post_options.path );
-                        post_request.write(post_data);
-                        post_request.end();
-                    }
-                });                
+                                // successful response
+                                // console.log(data.Reservations[0].Instances[0]);        
+                                var PublicIpAddress = data.Reservations[0].Instances[0].PublicIpAddress;
+                                // console.log("Host: ", JSON.stringify( PublicIpAddress ) );
+        
+                                // post the payload from GitHub
+                                var post_data = JSON.stringify(message);
+        
+                                // console.log("post_data length: ", JSON.stringify( post_data.length ) );
+                                
+                                // An object of options to indicate where to post to
+                                var post_options = {
+                                    host: PublicIpAddress,
+                                    port: '8101',
+                                    path: '/Deployment/Start/APP2?source=sourceName',
+                                    method: 'POST',
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                        'Content-Length': post_data.length
+                                    }
+                                };
+                            
+                                // Async call
+                                var post_request = http.request(post_options, function(res) {
+                                    // var body = '';
+                        
+                                    if (res.statusCode === 200) {
+                                        successCodes++;
+                                        console.log('Application update successfully deployed by Lambda function to ' + post_options.host);
+                                        // console.log('successCodes: ' + successCodes + ' instanceCount: ' + instanceCount);
+                                        if ( successCodes >= instanceCount ){
+                                            console.log('Application update successfully deployed to stack ' + paramsListRRS.StartRecordName);  
+                                            callback(null, 'Application update successfully deployed to stack ' + paramsListRRS.StartRecordName);  
+                                        }
+                                    } else {
+                                        var error = new Error('Error ' + res.statusCode + ' posting to ' + post_options.host);
+                                        callback(error ); 
+                                    }                        
+                                    
+                                    // res.on('data', function(chunk)  {
+                                    //     body += chunk;
+                                    // });
+                            
+                                    res.on('end', function() {
+                                        console.log( 'end ' + post_options.host );
+                                    });
+                            
+                                    res.on('error', function(e) {
+                                        callback(e, 'error posting to EC2 instance:');                                         
+                                    });
+                                });    
+                                // post the data
+                                console.log( 'Posting to:', post_options.host, post_options.path );
+                                post_request.write(post_data);
+                                post_request.end();
+                            }
+                        });                
+                    });
+                }
             });
         }
-    });
-    
+    });    
 };
