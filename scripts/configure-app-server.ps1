6aaa60fd6bd8dcfa56a440c18b8aa84e754fa8e0
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-06-01 15:26:33 +1000

    159678 Reduce number of prestarted web jobs

--- scripts/configure-app-server.ps1
+++ scripts/configure-app-server.ps1
@@ -3,8 +3,8 @@
 For ( $i = 1; $i -le 10; $i++) {
     $regkeyfolder = "HKLM:\Software\LANSA\C:%5CPROGRAM%20FILES%20(X86)%5CAPP$($i)\LANSAWEB"
     $regkeyname = "FREESLOTS"
-    Write-Output("Setting $RegKeyFolder $RegKeyName Ready To Use Minimum to 20")
-    New-ItemProperty -Path $regkeyfolder  -Name $regkeyname -Value '20' -PropertyType String -Force  | Out-Null
+    Write-Output("Setting $RegKeyFolder $RegKeyName Ready To Use Minimum to 5")
+    New-ItemProperty -Path $regkeyfolder  -Name $regkeyname -Value '5' -PropertyType String -Force  | Out-Null
 
     $regkeyname = "REUSE"
     Write-Output("Setting $RegKeyFolder $RegKeyName To Maximum")
