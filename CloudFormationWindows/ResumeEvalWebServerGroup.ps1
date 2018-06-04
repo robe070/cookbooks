de2fa7a1b42c2ba5dd78ecd0f708e4da1cc4c207
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-06-04 20:02:57 +1000

    Add in DBWebServerGroup

--- CloudFormationWindows/ResumeEvalWebServerGroup.ps1
+++ CloudFormationWindows/ResumeEvalWebServerGroup.ps1
@@ -7,3 +7,13 @@ foreach ( $stack in $stacks ) {
     $stack.ResourceId
     Resume-ASProcess -Region $Region -AutoScalingGroupName $stack.ResourceId
 }
+
+Write-Output 'DBWebServerGroup'
+$DBWebServerGroups = @(Get-ASTag -Region $Region -Filter @( @{ Name="key"; Values=@("aws:cloudformation:logical-id") } )) | Where-Object {$_.Value -eq 'DBWebServerGroup'}
+foreach ( $DBWebServerGroup in $DBWebServerGroups ) {
+    $DBWebServerGroup.ResourceId
+
+    # Resume most processes - Leave ReplaceUnhealthy suspended - Ensure that the instance is not terminated just because it is momentarily being updated.
+    # Done this way instead of resuming All and suspending RemoveUnhealthy so that any instances that are unhealthy are not replaced.
+    Resume-ASProcess -Region $Region -AutoScalingGroupName $DBWebServerGroup.ResourceId -ScalingProcess @("Launch", "AlarmNotification", "HealthCheck", "AZRebalance", "ScheduledActions", "AddToLoadBalancer", "Terminate", "RemoveFromLoadBalancerLowPriority")
+}
