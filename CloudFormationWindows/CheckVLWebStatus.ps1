3739af9f2285945b53704875b2ecc3691b81012f
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-07-13 11:23:15 +1000

    160274 Implement continuous monitoring of the stacks

--- CloudFormationWindows/CheckVLWebStatus.ps1
+++ CloudFormationWindows/CheckVLWebStatus.ps1
@@ -7,6 +7,31 @@
 
 'CheckVLWebStatus.ps1'
 
+function Summary {
+    If ( !$StackError ) {
+        Write-Host "All Apps in stacks " -NoNewline
+        $first = $true
+        foreach ( $stack in $stacklist) {
+            if ( $first ) {
+                $first = $false
+            } else {
+                Write-Host ', ' -NoNewline
+            }
+            Write-Host $Stack -NoNewline
+        }
+        Write-Host " are in service"
+    } else {
+        Write-Output "" | Out-Host
+        if ( $404count -gt 0 -or $defaultcount -gt 0  ) {
+            Write-RedOutput "Test failed"  | Out-Host
+        }
+        if ( $404count -gt 0 ){ Write-RedOutput "404 usually means the Listener is not running this is important to fix ASAP. And its simple to fix. Just re-deploy the app"}
+        if ( $500count -gt 0 ){ Write-FormattedOutput "500 usually means Free Trial was installed but no app was deployed. Look at git repo and check that there is just the one commit. If thats the case then this error may be ignored." -ForegroundColor 'yellow'}
+        if ( $defaultcount -gt 0 ){ Write-FormattedOutput "Other response codes have unknown cause" -ForegroundColor 'magenta'}
+    }    
+    Write-Host ""
+}
+
 $script:IncludeDir = $null
 if ( !$script:IncludeDir)
 {
@@ -30,82 +55,89 @@ Write-Host "$($a.ToUniversalTime()) UTC"
 
 try {
     $Region = 'us-east-1'
-    $404count = 0
-    $500count = 0
-    $defaultcount = 0
-    $StackStart = 1
-    $StackEnd = 9
+    $Perpetual = $true
+
+    $StackStart = 10
+    $StackEnd = 1
     [System.Collections.ArrayList]$stacklist = @()
     For ( $stack = $StackStart; $stack -le $StackEnd; $stack++) {
         $stacklist.add($stack) | Out-Null 
     }
-    $stacklist.add(10) | Out-Null
-    $stacklist.add(20) | Out-Null
+    #$stacklist.add(10) | Out-Null
+    #$stacklist.add(20) | Out-Null
     $stacklist.add(30) | Out-Null
 
-    $StackError = $false
+    $Loop = 0
+    do {
+        $StackError = $false
+        $FoundInstance = $false
+        $404count = 0
+        $500count = 0
+        $defaultcount = 0
+        $Loop++
 
-    $FoundInstance = $false
-    # Traverse each ASG for each stack to enumerate all the instances
-    for ($j = 1; $j -le 2; $j++) {
-        # Use a numbered loop so that individual stacks can be updated
-        foreach ( $stack in $stacklist) {
-            # Match on stack name too
-            if ( $J -eq 1 ){
-                $match = "eval$stack-DB*"
-            } else {
-                $match = "eval$stack-Web*"
-            }
+        # Traverse each ASG for each stack to enumerate all the instances
+        for ($j = 1; $j -le 2; $j++) {
+            # Use a numbered loop so that individual stacks can be updated
+            foreach ( $stack in $stacklist) {
+                # Match on stack name too
+                if ( $J -eq 1 ){
+                    $match = "eval$stack-DB*"
+                } else {
+                    $match = "eval$stack-Web*"
+                }
 
-            Write-GreenOutput $match | Out-Host
-            $StackInstances = @(Get-ASAutoScalingInstance -Region $Region | where-object {$_.AutoScalingGroupName -like $match } )
+                Write-GreenOutput $match | Out-Host
+                $StackInstances = @(Get-ASAutoScalingInstance -Region $Region | where-object {$_.AutoScalingGroupName -like $match } )
 
-            if ( $StackInstances){
-                $FoundInstance = $true
-                foreach ( $StackInstance in $StackInstances ) {
-                    $EC2Detail = Get-EC2Instance -Region $Region $StackInstance.InstanceId
+                if ( $StackInstances){
+                    $FoundInstance = $true
+                    foreach ( $StackInstance in $StackInstances ) {
+                        $EC2Detail = Get-EC2Instance -Region $Region $StackInstance.InstanceId
 
-                    $IPAddress = $Ec2Detail[0].Instances[0].PublicIpAddress
-                    Write-Host "EC2 $($Ec2Detail[0].Instances[0].InstanceId) $IPAddress" -NoNewline
-                    if ( $stack -eq 20 ) {
-                        $max = 5
-                    } else {
-                        $max = 10
+                        $IPAddress = $Ec2Detail[0].Instances[0].PublicIpAddress
+                        Write-Host "$Loop $($(Get-Date).ToLocalTime()) Local Time EC2 $($Ec2Detail[0].Instances[0].InstanceId) $IPAddress" -NoNewline
+                        if ( $stack -eq 20 ) {
+                            $max = 5
+                        } else {
+                            $max = 10
+                        }
+                        for ( $appl = 1; $appl -le $max; $appl++ ) {
+                            Write-Host -NoNewline " $appl"
+                            try {
+                                $url = "http://$IPAddress/app$appl/lansaweb?w=XVLSMTST&r=GETRESPONSE&vlweb=1&part=dem&lang=ENG"
+                                $response = Invoke-WebRequest -Uri $url
+                                $ResponseCode = $response.StatusCode
+                                switch ($ResponseCode) {
+                                    200 { }
+                                    404 { Write-Host "";Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'red' | Out-Host; $StackError = $true; $404count++ }
+                                    500 { Write-Host "";Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'yellow' | Out-Host; $StackError = $true; $500count++ }
+                                    default { Write-Host "";Write-FormattedOutput"$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'Magenta' | Out-Host; $StackError = $true; $defaultcount++ }
+                                }              
+                            } catch {
+                                Write-Host ""
+                                $StackError = $true
+                                $ResponseCode = $_.Exception.Response.StatusCode.Value__
+                                switch ($ResponseCode) {
+                                    404 { Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'red' | Out-Host; $404count++ }
+                                    500 { Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'yellow' | Out-Host; $500count++ }
+                                    default { Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'Magenta' | Out-Host; $defaultcount++ }
+                                }               
+                            }
+                            if ( $appl -eq 1) {
+                                # Workaround for IIS Plugin not coping with too many requests when first starting up
+                                # First request causes all the listener connections to be setup
+                                # 15 second is too short. Still get failures.
+                                Start-Sleep 0
+                            }
+                        }     
+                        Write-Host ""
                     }
-                    for ( $appl = 1; $appl -le $max; $appl++ ) {
-                        Write-Host -NoNewline " $appl"
-                        try {
-                            $url = "http://$IPAddress/app$appl/lansaweb?w=XVLSMTST&r=GETRESPONSE&vlweb=1&part=dem&lang=ENG"
-                            $response = Invoke-WebRequest -Uri $url
-                            $ResponseCode = $response.StatusCode
-                            switch ($ResponseCode) {
-                                200 { }
-                                404 { Write-Host "";Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'red' | Out-Host; $StackError = $true; $404count++ }
-                                500 { Write-Host "";Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'yellow' | Out-Host; $StackError = $true; $500count++ }
-                                default { Write-Host "";Write-FormattedOutput"$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'Magenta' | Out-Host; $StackError = $true; $defaultcount++ }
-                            }              
-                        } catch {
-                            Write-Host ""
-                            $StackError = $true
-                            $ResponseCode = $_.Exception.Response.StatusCode.Value__
-                            switch ($ResponseCode) {
-                                404 { Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'red' | Out-Host; $404count++ }
-                                500 { Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'yellow' | Out-Host; $500count++ }
-                                default { Write-FormattedOutput "$ResponseCode Stack $stack App $appl $url" -ForegroundColor 'Magenta' | Out-Host; $defaultcount++ }
-                            }               
-                        }
-                        if ( $appl -eq 1) {
-                            # Workaround for IIS Plugin not coping with too many requests when first starting up
-                            # First request causes all the listener connections to be setup
-                            # 15 second is too short. Still get failures.
-                            Start-Sleep 30
-                        }
-                    }     
-                    Write-Host ""
                 }
             }
         }
-    }
+        Summary
+    } while ($Perpetual -and $FoundInstance)
 
     if ( -not $FoundInstance ) {
         $StackError = $true
@@ -114,26 +146,4 @@ try {
     }
 } catch {
     $_
-} finally {
-    If ( !$StackError ) {
-        Write-Host "All Apps in stacks " -NoNewline
-        $first = $true
-        foreach ( $stack in $stacklist) {
-            if ( $first ) {
-                $first = $false
-            } else {
-                Write-Host ', ' -NoNewline
-            }
-            Write-Host $Stack -NoNewline
-        }
-        Write-Host " are in service"
-    } else {
-        Write-Output "" | Out-Host
-        if ( $404count -gt 0 -or $defaultcount -gt 0  ) {
-            Write-RedOutput "Test failed"  | Out-Host
-        }
-        if ( $404count -gt 0 ){ Write-RedOutput "404 usually means the Listener is not running this is important to fix ASAP. And its simple to fix. Just re-deploy the app"}
-        if ( $500count -gt 0 ){ Write-FormattedOutput "500 usually means Free Trial was installed but no app was deployed. Look at git repo and check that there is just the one commit. If thats the case then this error may be ignored." -ForegroundColor 'yellow'}
-        if ( $defaultcount -gt 0 ){ Write-FormattedOutput "Other response codes have unknown cause" -ForegroundColor 'magenta'}
-    }
 }
