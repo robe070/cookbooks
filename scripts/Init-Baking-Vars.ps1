f642c196f33e99a5de83e54114be5ee2da0b3f36
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-09-04 13:54:49 +1000

    160274 Fix cmd line utils outputting utf-16

--- scripts/Init-Baking-Vars.ps1
+++ scripts/Init-Baking-Vars.ps1
@@ -29,6 +29,14 @@ $Script:DVDDir = 'c:\LanDvdCut'
 # Make non-terminating errors into terminating errors. That is, the script will throw an exception so we know its gone wrong
 $ErrorActionPreference = 'Stop'
 
+# Make ALL encodings default to utf8. This is to fix command line utilities like pskill producing utf-16
+# output which CloudFormation cannot handle.
+# This syntax is PowerShell >= 3.0 compatible
+# A better solution is just to effect >, >> and out-file but it requires PS 5.1
+# Once win2012 support is no longer required, this should be changed
+# $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
 cmd /c exit 0       # Set $LASTEXITCODE so it always exists. Saves testing it everywhere
 
 Write-Debug "Variables loaded"
