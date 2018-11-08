da4e93277541c8d5f20fbcf85034b4452c1f3bff
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-11-08 16:44:29 +1100

    161062 Scripts to automate the resetting of a cloud environment

--- CloudFormationWindows/ReinstallAppInStack.ps1
+++ CloudFormationWindows/ReinstallAppInStack.ps1
@@ -1,4 +1,4 @@
-# Reinstall a specific app in stack 
+# Reinstall a specific app in stack
 
 param(
 [Parameter(Mandatory=$true)]
@@ -25,25 +25,27 @@ if ( !$script:IncludeDir)
 }
 else
 {
-	Write-Output "$(Log-Date) Environment already initialised" | Out-Host
+	Write-Output "$(Log-Date) Environment already initialised" | Write-Host
 }
 
 $a = Get-Date
 Write-Host "$($a.ToLocalTime()) Local Time"
 Write-Host "$($a.ToUniversalTime()) UTC"
 
+# Generate random  number to ensure (almost) a difference when updating the stack
+$Random = Get-Random
 try {
     $Region = 'us-east-1'
 
     & (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "SuspendStack.ps1") -Stack $Stack
-    
-    Write-GreenOutput( "$(Get-Date) Re-installing Application $App in stack $stack") | Out-Host
+
+    Write-GreenOutput( "$(Get-Date) Re-installing Application $App in stack $stack") | Write-Host
 
     aws cloudformation update-stack --stack-name eval$($stack) --region us-east-1 --capabilities CAPABILITY_IAM --use-previous-template `
     --parameters ParameterKey=03ApplCount,UsePreviousValue=true ParameterKey=03DBUsername,UsePreviousValue=true ParameterKey=04DBPassword,UsePreviousValue=true ParameterKey=05WebUser,UsePreviousValue=true ParameterKey=06WebPassword,UsePreviousValue=true ParameterKey=07KeyName,UsePreviousValue=true ParameterKey=08RemoteAccessLocation,UsePreviousValue=true ParameterKey=10LansaGitRepoBranch,UsePreviousValue=true ParameterKey=11WebServerInstanceTyp,UsePreviousValue=true ParameterKey=12WebServerMaxConnec,UsePreviousValue=true ParameterKey=13DBInstanceClass,UsePreviousValue=true ParameterKey=14DBName,UsePreviousValue=true ParameterKey=15DBEngine,UsePreviousValue=true ParameterKey=18WebServerCapacity,UsePreviousValue=true ParameterKey=19DBAllocatedStorage,UsePreviousValue=true ParameterKey=20DBIops,UsePreviousValue=true ParameterKey=DomainName,UsePreviousValue=true ParameterKey=DomainPrefix,UsePreviousValue=true ParameterKey=StackNumber,UsePreviousValue=true ParameterKey=WebServerGitRepo,UsePreviousValue=true `
     ParameterKey=22AppToReinstall,UsePreviousValue=false,ParameterValue=$App `
-    ParameterKey=22TriggerAppReinstall,UsePreviousValue=false,ParameterValue=$App `
-    ParameterKey=22TriggerAppUpdate,UsePreviousValue=true ParameterKey=22TriggerCakeUpdate,UsePreviousValue=true ParameterKey=23TriggerChefUpdate,UsePreviousValue=true ParameterKey=24TriggerWinUpdate,UsePreviousValue=true ParameterKey=25TriggerWebConfig,UsePreviousValue=true ParameterKey=26TriggerIcingUpdate,UsePreviousValue=true ParameterKey=01LansaMSI,UsePreviousValue=true ParameterKey=02LansaMSIBitness,UsePreviousValue=true ParameterKey=03ApplMSIuri,UsePreviousValue=true ParameterKey=17UserScriptHook,UsePreviousValue=true ParameterKey=19HostRoutePortNumber,UsePreviousValue=true ParameterKey=19HTTPPortNumber,UsePreviousValue=true ParameterKey=19HTTPPortNumberHub,UsePreviousValue=true ParameterKey=19JSMAdminPortNumber,UsePreviousValue=true ParameterKey=19JSMPortNumber,UsePreviousValue=true ParameterKey=21ELBTimeout,UsePreviousValue=true ParameterKey=27TriggerPatchInstall,UsePreviousValue=true ParameterKey=28PatchBucketName,UsePreviousValue=true ParameterKey=29PatchFolderName,UsePreviousValue=true ParameterKey=SSLCertificateARN,UsePreviousValue=true | Out-Host
+    ParameterKey=22TriggerAppReinstall,UsePreviousValue=false,ParameterValue=$Random `
+    ParameterKey=22TriggerAppUpdate,UsePreviousValue=true ParameterKey=22TriggerCakeUpdate,UsePreviousValue=true ParameterKey=23TriggerChefUpdate,UsePreviousValue=true ParameterKey=24TriggerWinUpdate,UsePreviousValue=true ParameterKey=25TriggerWebConfig,UsePreviousValue=true ParameterKey=26TriggerIcingUpdate,UsePreviousValue=true ParameterKey=01LansaMSI,UsePreviousValue=true ParameterKey=02LansaMSIBitness,UsePreviousValue=true ParameterKey=03ApplMSIuri,UsePreviousValue=true ParameterKey=17UserScriptHook,UsePreviousValue=true ParameterKey=19HostRoutePortNumber,UsePreviousValue=true ParameterKey=19HTTPPortNumber,UsePreviousValue=true ParameterKey=19HTTPPortNumberHub,UsePreviousValue=true ParameterKey=19JSMAdminPortNumber,UsePreviousValue=true ParameterKey=19JSMPortNumber,UsePreviousValue=true ParameterKey=21ELBTimeout,UsePreviousValue=true ParameterKey=27TriggerPatchInstall,UsePreviousValue=true ParameterKey=28PatchBucketName,UsePreviousValue=true ParameterKey=29PatchFolderName,UsePreviousValue=true ParameterKey=SSLCertificateARN,UsePreviousValue=true | Write-Host
     Write-Host( "*********************************************")
 
     if ( $LASTEXITCODE -ne 0 ) {
@@ -51,23 +53,30 @@ try {
     }
 
     Write-Host( "Waiting up to 10 minutes for template changes to be applied to stack eval$($stack)")
-    Wait-CFNStack -StackName "eval$($stack)" -region 'us-east-1' -Status 'UPDATE_COMPLETE' -Timeout 600 | Out-Host
+    Wait-CFNStack -StackName "eval$($stack)" -region 'us-east-1' -Status 'UPDATE_COMPLETE' -Timeout 600 | Write-Host
 
     # Wait for application to NOT be ready before checking its back online
-    Write-GreenOutput( "Wait until Stack eval$($stack) app $app is NOT ready before checking if its back online") | Out-Host
+    Write-GreenOutput( "Wait until Stack eval$($stack) app $app is NOT ready before checking if its back online") | Write-Host
     & "$script:InvocationDir\Wait-LansaApp.ps1" -WaitNotReady -Region $Region -Stack "eval$stack" -App $app -Timeout 300
 
-    Write-Host( "$(Get-Date) Wait for 10 minutes so that no activity is occurring whilst install is occurring...")
-    Start-Sleep -S 600
+    # Write-Host( "$(Get-Date) Wait for 10 minutes so that no activity is occurring whilst install is occurring...")
+    # Start-Sleep -S 600
 
-    Write-GreenOutput( "Wait until Stack eval$($stack) app $app is back online") | Out-Host
+    Write-GreenOutput( "Wait until Stack eval$($stack) app $app is back online") | Write-Host
     & "$script:InvocationDir\Wait-LansaApp.ps1" -WaitReady -Region $Region -Stack "eval$stack" -App $app -Timeout 1200
 
     & (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "ResumeStack.ps1") -Stack $Stack
 
-    Write-GreenOutput( "Stack eval$($stack) app $app reinstall is fully completed" ) | Out-Host
+    Write-GreenOutput( "Stack eval$($stack) app $app reinstall is fully completed" ) | Write-Host
 } catch {
-    $_
-    Write-Output ("If the error is 'An error occurred (ValidationError) when calling the UpdateStack operation: No updates are to be performed.', its because the previous application updated in this stack is the same one - there are no changes.") | Out-Host
+    Write-Host( "Exception")
+    $_ | Write-Host
+    Write-Host ("If the error is 'An error occurred (ValidationError) when calling the UpdateStack operation: No updates are to be performed.', its because the previous application updated in this stack is the same one - there are no changes.") | Write-Host
     throw
+    $e = $_.Exception
+    $e | format-list -force
+    Write-Host( "Configuration failed" )
+    cmd /c exit -1 | Write-Host    #Set $LASTEXITCODE
+    Write-Host( "LASTEXITCODE $LASTEXITCODE" )
+    return
 }
