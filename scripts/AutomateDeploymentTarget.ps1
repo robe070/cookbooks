40c304bebc2b3e0077d0f4a89f165589c4d5f798
Rob Goodridge <rob.goodridge@lansa.com.au>
2018-12-14 10:22:59 +1100

    161167 Allow database server to be specified

--- scripts/AutomateDeploymentTarget.ps1
+++ scripts/AutomateDeploymentTarget.ps1
@@ -1,6 +1,9 @@
 # Specify empty Database Password to use Windows Authentication to connect to the database
 param(
     [Parameter(Mandatory=$false)]
+    [String] $DatabaseServer=$($ENV:COMPUTERNAME),
+
+    [Parameter(Mandatory=$false)]
     [String] $DatabaseUser='lansa',
 
     [Parameter(Mandatory=$true)]
@@ -8,7 +11,7 @@ param(
 
     [Parameter(Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
-    [String] $WebUser='pcxuser3',
+    [String] $WebUser='pcxuser2',
 
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
@@ -67,8 +70,10 @@ else
 }
 
 Write-Host( "Parameter values...")
+
+Write-Host( "DatabaseServer = $DatabaseServer")
 Write-Host( "DatabaseUser = $DatabaseUser")
-Write-Host( "WebUser = $DatabaseUser")
+Write-Host( "WebUser = $WebUser")
 Write-Host( "SystemBranch = $SystemBranch")
 Write-Host( "app1RepoUrl = $app1RepoUrl")
 Write-Host( "app1Branch = $app1Branch")
@@ -175,8 +180,8 @@ try {
 
     cmd /c exit 0
 
-    $CommonParams = @("-server_name $($ENV:COMPUTERNAME)", "-dbut MSSQLS", "-dbuser $DatabaseUser", "-dbpassword `"$($DBCredentials.GetNetworkCredential().Password)`"", "-webuser $WebUser", "-webpassword $($WebCredentials.GetNetworkCredential().Password)", "-f32bit $f32bit", "-HTTPPortNumber 80", "-HTTPPortNumberHub 8101", "-HostRoutePortNumber 4540", "-JSMPortNumber 4561", "-JSMAdminPortNumber 4581", "-SUDB 1", "-UPGD false")
-    $Arguments = $CommonParams + @("-MSIUri $BaseMSIuri/WEBSERVR_v1.0.0_en-us.msi", "-ApplName $ApplName", "-dbname webserver", "-gitrepourl https://github.com/lansa/webserver.git", "-DisableSQLServer 0")
+    $CommonParams = @("-server_name $DatabaseServer", "-dbut MSSQLS", "-dbuser $DatabaseUser", "-dbpassword `"$($DBCredentials.GetNetworkCredential().Password)`"", "-webuser $WebUser", "-webpassword $($WebCredentials.GetNetworkCredential().Password)", "-f32bit $f32bit", "-HTTPPortNumber 80", "-HTTPPortNumberHub 8101", "-HostRoutePortNumber 4540", "-JSMPortNumber 4561", "-JSMAdminPortNumber 4581", "-SUDB 1", "-UPGD false")
+    $Arguments = $CommonParams + @("-MSIUri $BaseMSIuri/WEBSERVR_v1.0.0_en-us.msi", "-ApplName $ApplName", "-dbname webserverauto", "-gitrepourl https://github.com/lansa/webserver.git", "-DisableSQLServer 0")
     $Arguments
     Invoke-Expression "& $script:IncludeDir\install-lansa-msi.ps1 $Arguments"
 
