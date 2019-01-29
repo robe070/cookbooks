0c160f8f19f675838f7135379aea1cafa90d885d
Rob Goodridge <rob.goodridge@lansa.com.au>
2019-01-30 09:56:46 +1100

    Use $Region everywhere and wait for any kind of stack completion message and then check if it was successful

--- CloudFormationWindows/ReinstallAppInStack.ps1
+++ CloudFormationWindows/ReinstallAppInStack.ps1
@@ -41,7 +41,7 @@ try {
 
     Write-GreenOutput( "$(Get-Date) Re-installing Application $App in stack $stack") | Write-Host
 
-    aws cloudformation update-stack --stack-name eval$($stack) --region us-east-1 --capabilities CAPABILITY_IAM --use-previous-template `
+    aws cloudformation update-stack --stack-name eval$($stack) --region $Region --capabilities CAPABILITY_IAM --use-previous-template `
     --parameters ParameterKey=03ApplCount,UsePreviousValue=true ParameterKey=03DBUsername,UsePreviousValue=true ParameterKey=04DBPassword,UsePreviousValue=true ParameterKey=05WebUser,UsePreviousValue=true ParameterKey=06WebPassword,UsePreviousValue=true ParameterKey=07KeyName,UsePreviousValue=true ParameterKey=08RemoteAccessLocation,UsePreviousValue=true ParameterKey=10LansaGitRepoBranch,UsePreviousValue=true ParameterKey=11WebServerInstanceTyp,UsePreviousValue=true ParameterKey=12WebServerMaxConnec,UsePreviousValue=true ParameterKey=13DBInstanceClass,UsePreviousValue=true ParameterKey=14DBName,UsePreviousValue=true ParameterKey=15DBEngine,UsePreviousValue=true ParameterKey=18WebServerCapacity,UsePreviousValue=true ParameterKey=19DBAllocatedStorage,UsePreviousValue=true ParameterKey=20DBIops,UsePreviousValue=true ParameterKey=DomainName,UsePreviousValue=true ParameterKey=DomainPrefix,UsePreviousValue=true ParameterKey=StackNumber,UsePreviousValue=true ParameterKey=WebServerGitRepo,UsePreviousValue=true `
     ParameterKey=22AppToReinstall,UsePreviousValue=false,ParameterValue=$App `
     ParameterKey=22TriggerAppReinstall,UsePreviousValue=false,ParameterValue=$Random `
@@ -53,7 +53,14 @@ try {
     }
 
     Write-Host( "Waiting up to 10 minutes for template changes to be applied to stack eval$($stack)")
-    Wait-CFNStack -StackName "eval$($stack)" -region 'us-east-1' -Status 'UPDATE_COMPLETE' -Timeout 600 | Write-Host
+    Wait-CFNStack -StackName "eval$($stack)" -region $Region -Timeout 600 | Write-Host
+    $stackobject = get-cfnstack "eval$($stack)" -region $Region
+    if ($stackobject.StackStatus -ne 'UPDATE_COMPLETE') {
+        throw "Stack update failed"
+    }
+
+    Write-Host( "Check if the update was successful")
+
 
     # Wait for application to NOT be ready before checking its back online
     Write-GreenOutput( "Wait until Stack eval$($stack) app $app is NOT ready before checking if its back online") | Write-Host
