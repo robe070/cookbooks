f362a4d7b605bd1a5cf60ce9659e664dde251c2f
Rob Goodridge <rob.goodridge@lansa.com.au>
2019-02-05 15:03:48 +1100

    161062 Allow the remote url to already exist and ensure its set to the correct target

--- CloudFormationWindows/InitOneLansaevalRepo.ps1
+++ CloudFormationWindows/InitOneLansaevalRepo.ps1
@@ -21,35 +21,55 @@ cmd /c exit 0 #Set $LASTEXITCODE
 # So, silence is the best option and manually determine how to handle each command.
 $ErrorActionPreference = "SilentlyContinue"
 
+Write-Host( "TargetEnvironmentUrl = $TargetEnvironmentUrl")
+Write-Host( "EnvironmentName = $EnvironmentName")
+Write-Host( "Directory = $Directory")
 
 try {
+    if ( !(Test-Path $Directory) ) {
+        throw "$Directory does not exist."
+    }
+
     push-location
     set-location $Directory
+    pwd | Write-Host
 
     Write-Host( "Getting latest changes...")
-    git remote get-url origin | Write-Host
+    git remote get-url origin *>&1 | Write-Host
     if ( $LASTEXITCODE -ne 0) {
         throw "git remote get-url origin LASTEXITCODE = $LASTEXITCODE"
     }
-    git pull | Write-Host
+
+    # Reset to the origin rather than pull in order to overwrite whatever is currently in this directory
+    $branch = git rev-parse --abbrev-ref HEAD
+    git reset --hard origin/$branch *>&1
     if ( $LASTEXITCODE -ne 0) {
         throw "git pull LASTEXITCODE = $LASTEXITCODE"
     }
 
     Write-Host( "Adding a reference to the remote...")
-    Write-Host( "Ignore already exists fatal error. Second and subsequent runs of this script will get an error on the next line, and the first time, if there is a real error here, a subsequent command will throw an error.")
-    &git remote add $environmentName $TargetEnvironmentUrl | Write-Host
-    if ( $LASTEXITCODE -ne 0 -and ($LASTEXITCODE -ne 128) ) {
-        throw "git remote add $environmentName $TargetEnvironmentUrl LASTEXITCODE = $LASTEXITCODE"
+
+    git remote get-url $environmentName *>&1 | Out-Null
+    if ( $LASTEXITCODE -eq 128 ) {
+        Write-Host( "Remote does not exist, so add it" )
+        &git remote add $environmentName $TargetEnvironmentUrl *>&1 | Write-Host
+        if ( $LASTEXITCODE -ne 0 ) {
+            throw "git remote add $environmentName $TargetEnvironmentUrl LASTEXITCODE = $LASTEXITCODE"
+        }
+    } else {
+        $remoteurl = git remote get-url $environmentName
+        Write-Host( "Remote $EnvironmentName already configured to $Remoteurl")
+
+        &git remote set-url $environmentName $TargetEnvironmentUrl *>&1 | Write-Host
+        if ( $LASTEXITCODE -ne 0 ) {
+            throw "git remote set-url $environmentName $TargetEnvironmentUrl LASTEXITCODE = $LASTEXITCODE"
+        }
     }
 
-    Write-Host( "Remote $EnvironmentName configured to...")
-    git remote get-url $environmentName | Write-Host
-    if ( $LASTEXITCODE -ne 0) {
-        throw "git remote get-url LASTEXITCODE = $LASTEXITCODE"
-    }
+    $remoteurl = git remote get-url $environmentName
+    Write-Host( "Remote $EnvironmentName now set to $remoteurl")
 
-    Write-Host( "Push the current branch to $EnvironmentName...")
+    Write-Host( "Push the current branch to $TargetEnvironmentUrl...")
     git push --force $environmentName | Write-Host
     if ( $LASTEXITCODE -ne 0) {
         throw "git push --force LASTEXITCODE = $LASTEXITCODE"
