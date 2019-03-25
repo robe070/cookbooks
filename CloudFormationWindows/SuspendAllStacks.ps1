d35808e0138d910d4dfe6bb4f1158d50cfbba767
Rob Goodridge <rob.goodridge@lansa.com.au>
2019-03-25 11:08:07 +1100

    Stack Admin

--- CloudFormationWindows/SuspendAllStacks.ps1
+++ CloudFormationWindows/SuspendAllStacks.ps1
@@ -1,19 +1,41 @@
 # Suspen all evaluation stacks to suspend scaling.
+Param(
+    [Parameter(Mandatory)]
+        [ValidateSet('All','KeepAlive','Custom')]
+        [string] $ScalingProcesses
+)
+
 "SuspendAllstacks.ps1"
+
+[System.Collections.ArrayList]$ProcessList = @()
+
+
+switch ( $StackType ) {
+    'All' {
+        # Do nothing - use empty list
+    }
+    'KeepAlive' {
+        $ProcessList = @("Terminate", "ReplaceUnhealthy")
+    }
+    'Custom' {
+        # Edit this to what ever you need
+        $ProcessList = @("Terminate", "ReplaceUnhealthy")
+    }
+}
 
 $Region = 'us-east-1'
 $WebServerGroups = @(Get-ASTag -Region $Region -Filter @( @{ Name="key"; Values=@("aws:cloudformation:logical-id") } )) | Where-Object {$_.Value -eq 'WebServerGroup'}
 foreach ( $WebServerGroup in $WebServerGroups ) {
     $WebServerGroup.ResourceId
 
-    # Suspend all processes
-    Suspend-ASProcess -Region $Region -AutoScalingGroupName $WebServerGroup.ResourceId
+    # Suspend processes which may cause EC2 instance to be terminated
+    Suspend-ASProcess -Region $Region -AutoScalingGroupName $WebServerGroup.ResourceId -ScalingProcess @("Terminate", "ReplaceUnhealthy")
 }
 
 Write-Output 'DBWebServerGroup'
 $DBWebServerGroups = @(Get-ASTag -Region $Region -Filter @( @{ Name="key"; Values=@("aws:cloudformation:logical-id") } )) | Where-Object {$_.Value -eq 'DBWebServerGroup'}
 foreach ( $DBWebServerGroup in $DBWebServerGroups ) {
     $DBWebServerGroup.ResourceId
-    # Suspend all processes
-    Suspend-ASProcess -Region $Region -AutoScalingGroupName $DBWebServerGroup.ResourceId
+    # Suspend processes which may cause EC2 instance to be terminated
+    Suspend-ASProcess -Region $Region -AutoScalingGroupName $DBWebServerGroup.ResourceId -ScalingProcess @("Terminate", "ReplaceUnhealthy")
 }
