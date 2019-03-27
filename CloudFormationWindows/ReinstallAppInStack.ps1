39deb968a3b5573d0ed67a057e9ed5140e1e5225
Rob Goodridge <rob.goodridge@lansa.com.au>
2019-03-27 15:16:44 +1100

    161903 Stop reinstall if already configuring the stack and test scripts to test it

--- CloudFormationWindows/ReinstallAppInStack.ps1
+++ CloudFormationWindows/ReinstallAppInStack.ps1
@@ -5,7 +5,10 @@ param(
 [Decimal]$Stack,
 
 [Parameter(Mandatory=$true)]
-[Decimal]$App
+[Decimal]$App,
+
+[Parameter(Mandatory=$false)]
+[boolean]$SuspendStack = $true
 )
 
 "ReinstallAppInStack.ps1"
@@ -57,7 +60,9 @@ try {
         throw "Timed out waiting for exclusive access to ReinstallAppInStack script"
     }
 
-    & (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "SuspendStack.ps1") -Stack $Stack
+    if ( $SuspendStack ) {
+        & (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "SuspendStack.ps1") -Stack $Stack
+    }
 
     Write-GreenOutput( "$(Get-Date) Re-installing Application $App in stack $stack") | Write-Host
 
@@ -93,7 +98,9 @@ try {
     Write-GreenOutput( "Wait until Stack eval$($stack) app $app is back online") | Write-Host
     & "$script:InvocationDir\Wait-LansaApp.ps1" -WaitReady -Region $Region -Stack "eval$stack" -App $app -Timeout 1200
 
-    & (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "ResumeStack.ps1") -Stack $Stack
+    if ( $SuspendStack ) {
+        & (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "ResumeStack.ps1") -Stack $Stack
+    }
 
     Write-GreenOutput( "Stack eval$($stack) app $app reinstall is fully completed" ) | Write-Host
 } catch {
--- scripts/reinstall-app-paas.ps1
+++ scripts/reinstall-app-paas.ps1
@@ -10,8 +10,8 @@ Installs Application n
 
 Requires the LANSA AMI Scalable license
 
-# N.B. It is vital that the user id and password supplied pass the password rules. 
-E.g. The password is sufficiently complex and the userid is not duplicated in the password. 
+# N.B. It is vital that the user id and password supplied pass the password rules.
+E.g. The password is sufficiently complex and the userid is not duplicated in the password.
 i.e. UID=PCXUSER and PWD=PCXUSER@#$%^&* is invalid as the password starts with the entire user id "PCXUSER".
 
 .EXAMPLE
@@ -50,7 +50,7 @@ if ( -not $script:IncludeDir)
 {
     # Log-Date can't be used yet as Framework has not been loaded
 
-	Write-Output "Initialising environment - presumed not running through RemotePS"
+	Write-Host "Initialising environment - presumed not running through RemotePS"
 	$MyInvocation.MyCommand.Path
 	$script:IncludeDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
@@ -59,12 +59,12 @@ if ( -not $script:IncludeDir)
 }
 else
 {
-	Write-Output "$(Log-Date) Environment already initialised - presumed running through RemotePS"
+	Write-Host "$(Log-Date) Environment already initialised - presumed running through RemotePS"
 }
 
-Write-Output "$(Log-Date) Reinstalling Application $ApplNumber in LANSA PaaS environment"
+Write-Host "$(Log-Date) Reinstalling Application $ApplNumber in LANSA PaaS environment"
 
-Write-Output("$(Log-Date) Script Directory: $script:IncludeDir")
+Write-Host("$(Log-Date) Script Directory: $script:IncludeDir")
 
 if ( $f32bit -eq 'true' -or $f32bit -eq '1')
 {
@@ -87,64 +87,87 @@ else
 cmd /c exit 0    #Set $LASTEXITCODE
 
 try {
+    Write-Host( "(Log-Date) Check if installation already in progress. If it is, wait 10 seconds and try again for up to 5 minutes")
+
+    $Installing = Get-ItemProperty -Path "HKLM:\Software\lansa" -Name 'Installing'
+    Write-Host( "Installing = $($Installing.Installing)" )
+    if ( $Installing.Installing -eq 1) {
+       Publish-SNSMessage 'arn:aws:sns:us-east-1:775488040364:NotifyMe1' -Message "Stack $StackNumber App $ApplNumber configuration change in progress. Waiting up to 5 minutes for configuration change to complete" -Region 'us-east-1' | Out-Default | Write-Host
+       for ($i = 0; $i -lt 3; $i++ ){
+           Start-Sleep 10
+           $Installing = Get-ItemProperty -Path "HKLM:\Software\lansa" -Name 'Installing'
+           if ( $Installing.Installing -eq 0) {
+               Publish-SNSMessage 'arn:aws:sns:us-east-1:775488040364:NotifyMe1' -Message "Stack $StackNumber App $ApplNumber configuration change completed"  -Region 'us-east-1' | Out-Default | Write-Host
+               break
+           }
+       }
+       if ( $Installing.Installing -eq 1) {
+           Publish-SNSMessage 'arn:aws:sns:us-east-1:775488040364:NotifyMe1' -Message "Stack $StackNumber App $ApplNumber configuration change NOT completed. Application re-installation NOT performed"  -Region 'us-east-1' | Out-Default | Write-Host
+           throw "Stack $StackNumber App 1 configuration change NOT completed. Application re-installation NOT performed"
+       }
+    }
+    Set-ItemProperty -Path "HKLM:\Software\lansa" -Name 'Installing' -Value 1 | Out-Default | Write-Host
+
     $ApplName = "WebServer"
     if ($f32bit_bool) {
         $APPA = "${ENV:ProgramFiles(x86)}\$($ApplName)"
     } else {
         $APPA = "${ENV:ProgramFiles}\$($ApplName)"
     }
-    Write-Output( "$(Log-Date) Companion Install Path $APPA" )
+    Write-Host( "$(Log-Date) Companion Install Path $APPA" )
 
 
     $ApplName = "APP$ApplNumber"
-    Write-Output( "$(Log-Date) Uninstalling application $ApplName")
-    & "$script:IncludeDir\uninstall-lansa-msi.ps1" -DBUT $DBUT -server_name $server_name -dbname $ApplName -dbuser $dbuser -dbpassword $dbpassword $webpassword -f32bit $f32bit -SUDB $SUDB -wait $wait -ApplName $ApplName -CompanionInstallPath $APPA    
+    Write-Host( "$(Log-Date) Uninstalling application $ApplName")
+    & "$script:IncludeDir\uninstall-lansa-msi.ps1" -DBUT $DBUT -server_name $server_name -dbname $ApplName -dbuser $dbuser -dbpassword $dbpassword $webpassword -f32bit $f32bit -SUDB $SUDB -wait $wait -ApplName $ApplName -CompanionInstallPath $APPA | Out-Default | Write-Host
 
-    Write-Output( "$(Log-Date) Installing $ApplName")
+    Write-Host( "$(Log-Date) Installing $ApplName")
 
     if ( $ApplNumber -le 9 ) {
         $RepoAppIndex = $ApplNumber
     } else {
         $RepoAppIndex = 0
-    } 
-    $GitRepoName = "lansaeval$StackNumber$RepoAppIndex"    
-    
-    Write-Output( "$(Log-Date) gitreponame $GitRepoName")
+    }
+    $GitRepoName = "lansaeval$StackNumber$RepoAppIndex"
 
-    & "$script:IncludeDir\install-lansa-msi.ps1" -server_name $server_name -dbname $ApplName -dbuser $dbuser -dbpassword $dbpassword -webuser $webuser -webpassword $webpassword -f32bit $f32bit -SUDB $SUDB -UPGD $UPGD -userscripthook $userscripthook -wait $wait -ApplName $ApplName -CompanionInstallPath $APPA -MSIuri "$ApplMSIuri/$($ApplName)_v1.0.0_en-us.msi" $HTTPPortNumber -HostRoutePortNumber $HostRoutePortNumber -JSMPortNumber $JSMPortNumber -JSMAdminPortNumber $JSMAdminPortNumber -HTTPPortNumberHub $HTTPPortNumberHub -GitRepoUrl "git@github.com:lansa/$($GitRepoName).git"    
-   
+    Write-Host( "$(Log-Date) gitreponame $GitRepoName")
+
+    & "$script:IncludeDir\install-lansa-msi.ps1" -server_name $server_name -dbname $ApplName -dbuser $dbuser -dbpassword $dbpassword -webuser $webuser -webpassword $webpassword -f32bit $f32bit -SUDB $SUDB -UPGD $UPGD -userscripthook $userscripthook -wait $wait -ApplName $ApplName -CompanionInstallPath $APPA -MSIuri "$ApplMSIuri/$($ApplName)_v1.0.0_en-us.msi" $HTTPPortNumber -HostRoutePortNumber $HostRoutePortNumber -JSMPortNumber $JSMPortNumber -JSMAdminPortNumber $JSMAdminPortNumber -HTTPPortNumberHub $HTTPPortNumberHub -GitRepoUrl "git@github.com:lansa/$($GitRepoName).git" | Out-Default | Write-Host
+
     if ($LASTEXITCODE -eq 0 ) {
         # No need to perform a reset as the web-alias and port numbers have not changed.
         # iisreset
     } else {
-        Write-Output( "$(Log-Date) throwing")
+        Write-Host( "$(Log-Date) throwing")
         throw
     }
 } catch {
     $e = $_.Exception
-    $e | format-list -force
- 
-    Write-Output( "Application $ApplName reinstallation failed" )
-    Write-Output( "Raw LASTEXITCODE $LASTEXITCODE" )
+    $e | format-list -force | Out-Default | Write-Host
+
+    Write-Host( "Application $ApplName reinstallation failed" )
+    Write-Host( "Raw LASTEXITCODE $LASTEXITCODE" )
     if ( ( -not [ string ]::IsNullOrWhiteSpace( $LASTEXITCODE ) ) -and ( $LASTEXITCODE -ne 0 ) )
     {
        $ExitCode = $LASTEXITCODE
-       Write-Output( "ExitCode set to LASTEXITCODE $ExitCode" )
+       Write-Host( "ExitCode set to LASTEXITCODE $ExitCode" )
     } else {
        $ExitCode = $e.HResult
-       Write-Output( "ExitCode set to HResult $ExitCode" )
+       Write-Host( "ExitCode set to HResult $ExitCode" )
     }
- 
+
     if ( $ExitCode -eq $null -or $ExitCode -eq 0 )
     {
        $ExitCode = -1
-       Write-Output( "ExitCode set to $ExitCode" )
+       Write-Host( "ExitCode set to $ExitCode" )
     }
-    Write-Output( "Final ExitCode $ExitCode" )
+    Write-Host( "Final ExitCode $ExitCode" )
     cmd /c exit $ExitCode    #Set $LASTEXITCODE
-    Write-Output( "Final LASTEXITCODE $LASTEXITCODE" )
+    Write-Host( "Final LASTEXITCODE $LASTEXITCODE" )
     return
+ } finally {
+    Set-ItemProperty -Path "HKLM:\Software\lansa" -Name 'Installing' -Value 0 | Out-Default | Write-Host
  }
- Write-Output( "Application $ApplName reinstallation succeeded" )
+ Write-Host( "Application $ApplName reinstallation succeeded" )
  cmd /c exit 0    #Set $LASTEXITCODE
- Write-Output( "LASTEXITCODE $LASTEXITCODE" )
+ Write-Host( "LASTEXITCODE $LASTEXITCODE" )
