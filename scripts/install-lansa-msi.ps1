19083b34b6ee0e88ea4915705c7e0c3dc40db378
Rob Goodridge <rob.goodridge@lansa.com.au>
2019-11-08 13:20:22 +1100

    161888 Fix up new image not having SQL Server installed

--- scripts/install-lansa-msi.ps1
+++ scripts/install-lansa-msi.ps1
@@ -130,16 +130,21 @@ try
     # ***********************************************************************************
     if ( (-not $CompanionInstall -and $DisableSQLServer ) ) {
         Write-Host( "$(Log-Date) Disable SQL Server service so it doesn't randomly start up" )
-        $service = @(get-service "MSSQLSERVER")
-        $count = $($service | Measure-Object).Count
-        if ( $Count -ne 1 ) {
-            $service  | Format-Table Name, DisplayName, Status, StartType, DependentServices, ServicesDependedOn | Out-Host
-            throw "Should only be one MSSQLSERVER service"
-        }
-        stop-service $service[0] -Force | Out-Default | Write-Host
-        set-service $Service[0].Name -StartupType Disabled | Out-Default | Write-Host
 
-        @(get-service "MSSQLSERVER") | Format-Table Name, DisplayName, Status, StartType, DependentServices, ServicesDependedOn | Out-Host
+        # Check if SQL Server is installed
+        $mssql_services = Get-WmiObject win32_service | where-object name -like 'MSSQL*'
+        If ( $null -ne $mssql_services ) {
+            $service = @(get-service "MSSQLSERVER")
+            $count = $($service | Measure-Object).Count
+            if ( $Count -ne 1 ) {
+                $service  | Format-Table Name, DisplayName, Status, StartType, DependentServices, ServicesDependedOn | Out-Host
+                throw "Should only be one MSSQLSERVER service"
+            }
+            stop-service $service[0] -Force | Out-Default | Write-Host
+            set-service $Service[0].Name -StartupType Disabled | Out-Default | Write-Host
+
+            @(get-service "MSSQLSERVER") | Format-Table Name, DisplayName, Status, StartType, DependentServices, ServicesDependedOn | Out-Host
+        }
     }
     # ***********************************************************************************
 
