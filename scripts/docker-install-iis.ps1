e7422f1536f8a50a1d8d627198ca4f1d3ae901f6
Rob Goodridge <rob.goodridge@lansa.com.au>
2019-12-05 10:29:39 +1100

    temporary code

--- scripts/docker-install-iis.ps1
+++ scripts/docker-install-iis.ps1
@@ -40,6 +40,7 @@ try {
 
     Write-Output "Turning off complex password requirements"
     secedit /export /cfg c:\secpol.cfg
+    return
     (Get-Content C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
     secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
     Remove-Item -force c:\secpol.cfg -confirm:$false
