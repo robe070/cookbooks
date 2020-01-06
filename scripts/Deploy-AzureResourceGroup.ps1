2772fe8a365bf83727e6991db580b9d6e72f3cea
Rob Goodridge <rob.goodridge@lansa.com.au>
2020-01-07 10:26:20 +1100

    v14.2.0.8 Azure image creation

---
+++ scripts/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,32 @@
+﻿#Requires -Version 3.0
+#Requires -Module Az.Resources
+#Requires -Module Az.Storage
+
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] $TemplateFile = '..\..\azure-quickstart-templates\lansa-vmss-windows-autoscale-sql-database\azuredeploy.json',
+    [string] $TemplateParameterFile = '.\azuredeploy.parameters.json'
+)
+
+Enable-AzureRmAlias
+
+# try {
+#     [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(" ","_"), "2.9")
+# } catch { }
+
+Set-StrictMode -Version 3
+
+$OptionalParameters = New-Object -TypeName Hashtable
+$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
+$TemplateParameterFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParameterFile))
+
+# Create or update the resource group using the specified template file and template parameters file
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop
+
+New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                   -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -TemplateParameterFile $TemplateParameterFile `
+                                   @OptionalParameters `
+                                   -Force -Verbose
