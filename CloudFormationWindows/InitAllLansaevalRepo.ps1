ced5a901110148c093b5ddbfeda84457e362002d
Rob Goodridge <rob.goodridge@lansa.com.au>
2020-05-13 18:19:18 +1000

    Example of pushing a different branch to a remote

--- CloudFormationWindows/InitAllLansaevalRepo.ps1
+++ CloudFormationWindows/InitAllLansaevalRepo.ps1
@@ -3,8 +3,8 @@
 push-location
 set-location 'c:\lansa\lansaeval2'
 
-$RepoStart = 100
-$RepoEnd = 99
+$RepoStart = 200
+$RepoEnd = 209
 [System.Collections.ArrayList]$Repolist = @()
 For ( $Repo = $RepoStart; $Repo -le $RepoEnd; $Repo++) {
     $Repolist.add($Repo) | Out-Null
@@ -16,6 +16,7 @@ foreach ( $Repo in $Repolist ) {
     #Next line only needed when first deploying from a git repo
     #git remote add lansaeval$($Repo) git@github.com:lansa/lansaeval$($Repo).git
     git remote get-url lansaeval$($Repo)
+    # git push --force lansaeval$($Repo) lansaeval301_master:master
     git push --force lansaeval$($Repo)
     Write-Output( "*********************************************")
 }
