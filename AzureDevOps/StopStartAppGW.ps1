897cd97d2ad743cbdc7ace955fdb57d58d76d641
Rob Goodridge <rob.goodridge@lansa.com.au>
2020-07-24 10:24:29 +1000

    163204 First fully running version of StopStartAppGW.ps1

--- AzureDevOps/StopStartAppGW.ps1
+++ AzureDevOps/StopStartAppGW.ps1
@@ -1,8 +1,8 @@
 <#PSScriptInfo
 
-.DESCRIPTION Azure Automation Workflow Runbook Script to stop or start all Application Gateways in the current subscription or in a specific Resource Group. Useful for dev and test environments. Written to be used as either a scheduled job at the close of business or ad hoc when Application Gateways are finished with for the moment. If the Application Gateway is tagged with ShutdownPolicy = Excluded, the Application Gateway is not stopped. Application Gateways are also not stopped if it is already managed by a schedule. Requires an Azure Automation account with an Azure Run As account credential.
+.DESCRIPTION Azure Automation Workflow Runbook Script to stop or start all Application Gateways in the current subscription or in a specific Resource Group. Useful for dev and test environments. Written to be used as either a scheduled job at the close of business or ad hoc when Application Gateways are finished with for the moment. If the Application Gateway is tagged with ShutdownPolicy = Excluded, the Application Gateway is not stopped. Requires an Azure Automation account with an Azure Run As account credential.
 
-.VERSION 1.0.2
+.VERSION 1.0.3
 
 .GUID 81441e5f-d154-4666-97cf-b8f3decb9341
 
@@ -27,7 +27,7 @@
 .RELEASENOTES
 1.0.1: - Add initial version
 1.0.2: - Gallery text changes
-
+1.0.3: - Use Connect-AzureAutomation module
 #>
 
 <#
@@ -41,6 +41,9 @@ The Azure resource group name or leave empty to target ALL Application Gateways 
 Specify either 'stop' or 'start' to stop or start the Application Gateways.
 #>
 
+#Requires -Modules Connect-AzureAutomation
+# import-module .\Connect-AzureAutomation
+
 workflow StopStartAppGW
 {
     Param
@@ -49,49 +52,7 @@ workflow StopStartAppGW
 	    [Parameter(Mandatory=$true)] [ValidateSet("Start","Stop")] [String]	$Action
     )
 
-# Use the Run As connection to login to Azure
-function Login-AzureAutomation([bool] $AzModuleOnly, [String]$AzureEnvironment = 'AzureCloud') {
-    try {
-        $RunAsConnection = Get-AutomationConnection -Name "AzureRunAsConnection"
-        Write-Output "Logging in to Azure ($AzureEnvironment)..."
-
-        if (!$RunAsConnection.ApplicationId) {
-            $ErrorMessage = "Connection 'AzureRunAsConnection' is incompatible type."
-            throw $ErrorMessage
-        }
-
-        if ($AzModuleOnly) {
-            Connect-AzAccount `
-                -ServicePrincipal `
-                -TenantId $RunAsConnection.TenantId `
-                -ApplicationId $RunAsConnection.ApplicationId `
-                -CertificateThumbprint $RunAsConnection.CertificateThumbprint `
-                -Environment $AzureEnvironment
-
-            Select-AzSubscription -SubscriptionId $RunAsConnection.SubscriptionID  | Write-Verbose
-        } else {
-            Add-AzureRmAccount `
-                -ServicePrincipal `
-                -TenantId $RunAsConnection.TenantId `
-                -ApplicationId $RunAsConnection.ApplicationId `
-                -CertificateThumbprint $RunAsConnection.CertificateThumbprint `
-                -Environment $AzureEnvironment
-
-            Select-AzureRmSubscription -SubscriptionId $RunAsConnection.SubscriptionID  | Write-Verbose
-        }
-    } catch {
-        if (!$RunAsConnection) {
-            $RunAsConnection | fl | Write-Output
-            Write-Output $_.Exception
-            $ErrorMessage = "Connection 'AzureRunAsConnection' not found."
-            throw $ErrorMessage
-        }
-
-        throw $_.Exception
-    }
-}
-
-    Login-AzureAutomation
+    Connect-AzureAutomation
 
     if ( $AzureResourceGroup ) {
         $AppGWs = @(Get-AzureRmApplicationGateway -ResourceGroupName $AzureResourceGroup)
@@ -107,7 +68,7 @@ function Login-AzureAutomation([bool] $AzModuleOnly, [String]$AzureEnvironment =
         Write-Output "Stopping Application Gateways"
         foreach -parallel ($AppGW in ($AppGWs) )
         {
-            if ($AppGW.OperationalState -eq "running" -and $AppGW.Tags["ShutdownPolicy"] -ne "Excluded" )  {
+            if ($AppGW.OperationalState -eq "running" -and (-not $AppGW.Tags -or $AppGW.Tags["ShutdownPolicy"] -ne "Excluded" ) ) {
                 Write-Output "Stopping Application Gateway '$($AppGW.ResourceGroupName)/$($AppGW.name)'"
                 Stop-AzureRmApplicationGateway -ApplicationGateway $AppGW -Verbose
             } else {
