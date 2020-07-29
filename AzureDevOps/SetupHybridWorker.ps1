117df3d98b3c0bc443b262f33f5573a171e6a5d3
Rob Goodridge <rob.goodridge@lansa.com.au>
2020-07-29 14:43:59 +1000

    163204 Setup Hybrid Worker

---
+++ AzureDevOps/SetupHybridWorker.ps1
@@ -0,0 +1,3 @@
+New-OnPremiseHybridWorker.ps1 -AutomationAccountName 'Automation' -AAResourceGroupName 'Automation'`
+-HybridGroupName 'RGHybridWorker' `
+-SubscriptionID '739c4e86-bd75-4910-8d6e-d7eb23ab94f3' -WorkspaceName 'RGHybridWorker'
