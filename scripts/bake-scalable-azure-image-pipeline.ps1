4cefa2f9a47cf87e6c966b44019ea2146e009b85
Varun Chopra <varun.chopra@accoliteindia.com>
2020-08-04 16:47:17 +0530

    163204: Generic script that Bake Azure Scalable Image * Used by Release Pipeline

---
+++ scripts/bake-scalable-azure-image-pipeline.ps1
@@ -0,0 +1,76 @@
+﻿<#
+.SYNOPSIS
+
+Generic script to Bake a LANSA image for Azure
+
+.DESCRIPTION
+Used by Image Pipeline
+.EXAMPLE
+
+
+#>
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $VersionText,
+
+    [Parameter(Mandatory=$true)]
+    [int]
+    $VersionMajor,
+
+    [Parameter(Mandatory=$true)]
+    [int]
+    $VersionMinor,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $AmazonAMIName,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $GitBranch,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $Cloud='AWS',
+
+    [Parameter(Mandatory=$false)]
+    [boolean]
+    $Win2012=$true
+    )
+
+# $DebugPreference = "Continue"
+$VerbosePreference = "Continue"
+
+$MyInvocation.MyCommand.Path
+$script:IncludeDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+. "$script:IncludeDir\Init-Baking-Vars.ps1"
+. "$script:IncludeDir\Init-Baking-Includes.ps1"
+
+. "$Script:IncludeDir\bake-ide-ami.ps1"
+
+###############################################################################
+# Main program logic
+###############################################################################
+
+Set-StrictMode -Version Latest
+Bake-IdeMsi -VersionText $VersionText `
+            -VersionMajor $VersionMajor `
+            -VersionMinor $VersionMinor `
+            -LocalDVDImageDirectory "\\devsrv\ReleasedBuilds\v14\SPIN0332_LanDVDcut_L4W14100_4138_160727_GA" `
+            -S3DVDImageDirectory "https://lansalpcmsdn.blob.core.windows.net/releasedbuilds/v14/LanDVDcut_L4W14000_latest" `
+            -S3VisualLANSAUpdateDirectory "https://lansalpcmsdn.blob.core.windows.net/releasedbuilds/v14/VisualLANSA_L4W14000_latest" `
+            -S3IntegratorUpdateDirectory "https://lansalpcmsdn.blob.core.windows.net/releasedbuilds/v14/Integrator_L4W14000_latest" `
+            -AmazonAMIName $AmazonAMIName `
+            -GitBranch $GitBranch `
+            -Cloud $Cloud `
+            -InstallBaseSoftware $true `
+            -InstallSQLServer $false `
+            -InstallIDE $false `
+            -InstallScalable $true `
+            -Win2012 $Win2012 `
+            -ManualWinUpd $false `
+            -SkipSlowStuff $false `
+            -OnlySaveImage $false `
+            -CreateVM $true
