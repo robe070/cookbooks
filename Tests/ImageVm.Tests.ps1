a69fa7a4c99e3b6cb1eef1409481bdf8b6d65f5a
praveen <dande.kumar@accoliteindia.com>
2020-12-08 16:22:56 +0530

    Version Text test

--- Tests/ImageVm.Tests.ps1
+++ Tests/ImageVm.Tests.ps1
@@ -14,8 +14,7 @@ Describe "VM Tests" {
         $ImgName = $env:TestImageName
         $ImgName | Out-Default | Write-Host
 
-        #$CloudName = $env:TestCloudName
-        $CloudName = "AWS"
+        $CloudName = $env:TestCloudName
         $CloudName | Out-Default | Write-Host
 
         $VMname = "TestImageVM"
@@ -151,15 +150,14 @@ Describe "VM Tests" {
             enable-psremoting -SkipNetworkProfileCheck -force
             set-item wsman:\localhost\Client\TrustedHosts -value * -force
             $VerbosePreference = $VerbosePreferenceSaved
-            #$imageId = $ImgName
-            $imageId = "ami-093075401ba5b44c4"
-            #$script:keypairfile = $env:keypairpath
-            #$script:keypair = $env:keypair
-            $script:keypairfile = "C:\Users\ADMIN\Downloads\Praveen.pem"
-            $script:keypair = 'Praveen'
+            $imageId = $ImgName
+            $script:keypairfile = $env:keypairpath
+            $script:keypair = $env:keypair
+            #$script:keypairfile = "C:\Users\ADMIN\Downloads\Praveen.pem"
+            #$script:keypair = 'Praveen'
             . "$script:IncludeDir\dot-AWSTools.ps1"
-            #$script:SG = $env:SG
-            $script:SG = "w12rd2-14-2-Test-DP"
+            $script:SG = $env:SG
+            #$script:SG = "w12rd2-14-2-Test-DP"
             Create-Ec2SecurityGroup
 
             $script:instancename = " Praveen Test LANSA Scalable License installed on $(Log-Date)"
