0f981f64f94d70a5fe05e77dc80453045960a065
Rob Goodridge <rob.goodridge@lansa.com.au>
2021-02-25 10:34:37 +1100

    163279 Change choco feed to a project feed to fix change in functionality in Azure DevOps organisational feeds which are now NOT public. Go figure.

---
+++ scripts/choco-push.ps1
@@ -0,0 +1,10 @@
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\kdiff3.0.9.98.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\chocolatey-core.extension.1.3.5.1.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\DotNet4.5.2.4.5.2.20140902.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\FoxitReader.10.1.1.37576.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\git.2.29.2.2.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\git.install.2.29.2.2.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\GoogleChrome.88.0.4324.190.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\jre8.8.0.271.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\vscode.1.51.1.nupkg
+nuget push -Source "choco" -ApiKey AzureDevOps C:\Users\Robert.SYD\Downloads\vscode.install.1.51.1.nupkg
