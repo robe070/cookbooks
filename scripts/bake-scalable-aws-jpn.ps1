33d511733ebad9dca16ead8b7a6ffc9e54e6840c
Rob Goodridge <rob.goodridge@lansa.com.au>
2021-04-01 16:12:40 +1100

    Revert "Use AWS JPN image to build LANSA JPN image"
    This reverts commit c5c20ab1881342e0b6bcc2b1cce70ac3d371b658.
    
    # Conflicts:
    #	scripts/bake-scalable-aws-jpn.ps1

--- scripts/bake-scalable-aws-jpn.ps1
+++ scripts/bake-scalable-aws-jpn.ps1
@@ -16,7 +16,7 @@ $VerbosePreference = "Continue"
 $MyInvocation.MyCommand.Path
 $Script:IncludeDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-. "$Script:IncludeDir\bake-ide-ami.ps1"
+. "$Script:IncludeDir\bake-jpn-ami.ps1"
 
 ###############################################################################
 # Main program logic
@@ -26,21 +26,21 @@ Set-StrictMode -Version Latest
 
 # Note that the first 3 characters of VersionText are important. w12, w16 or w19 to match the Windows version
 
-Bake-IdeMsi -VersionText 'w16jpn' `
+Bake-IdeMsi -VersionText 'w12jpn' `
             -VersionMajor 1 `
             -VersionMinor 0 `
             -LocalDVDImageDirectory "ignore" `
             -S3DVDImageDirectory "ignore" `
             -S3VisualLANSAUpdateDirectory "ignore" `
             -S3IntegratorUpdateDirectory "ignore" `
-            -AmazonAMIName "Windows_Server-2016-Japanese-Full-SQL_2016_SP2_Express*" `
+            -AmazonAMIName "LANSA Scalable License  w12r2d*" `
             -GitBranch "debug/jpn" `
             -Cloud "AWS" `
-            -InstallBaseSoftware $true `
+            -InstallBaseSoftware $false `
             -InstallSQLServer $false `
             -InstallIDE $false `
             -InstallScalable $true `
-            -Win2012 $false `
+            -Win2012 $true `
             -RunWindowsUpdates $false `
             -ManualWinUpd $false `
             -SkipSlowStuff $false `
