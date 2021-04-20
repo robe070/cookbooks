811a3d50193a7a36ace4e15cda01dd9ced6cb9e9
Rob Goodridge <rob.goodridge@lansa.com.au>
2021-04-20 10:10:46 +1000

    JPN Language Pack. Ignore error when restarting computer

--- scripts/language-pack-config-2.ps1
+++ scripts/language-pack-config-2.ps1
@@ -19,16 +19,18 @@ switch ( $Platform) {
                 Set-WinCultureFromLanguageListOptOut -OptOut $False
 
                 Write-Host( "Set the location to Japan")
+                # Is this appropriate? This image may be started in any region of the world. What does it mean to be 'in Japan' when you may be running anywhere?
                 Set-WinHomeLocation -GeoId 0x7A
 
                 Write-Host( "Set the system locale to Japan")
                 Set-WinSystemLocale -SystemLocale ja-JP
 
                 Write-Host( "Leave timezone as UTC")
-                #タイムゾーンを東京にします。
                 # Set-TimeZone -Id "Tokyo Standard Time"
 
-                Restart-Computer
+                # Ignore errors as it seems a shutdown is in progress anyway, possibly due to setting the System Locale.
+                # "Failed to restart the computer EC2AMAZ-1J2TNAU with the following error message: A system shutdown is in progress"
+                Restart-Computer -ErrorAction SilentlyContinue
             }
         }
     }
@@ -42,16 +44,18 @@ switch ( $Platform) {
                 Set-WinCultureFromLanguageListOptOut -OptOut $False
 
                 Write-Host( "Set the location to Japan")
+                # Is this appropriate? This image may be started in any region of the world. What does it mean to be 'in Japan' when you may be running anywhere?
                 Set-WinHomeLocation -GeoId 0x7A
 
                 Write-Host( "Set the system locale to Japan")
                 Set-WinSystemLocale -SystemLocale ja-JP
 
                 Write-Host( "Leave timezone as UTC")
-                #タイムゾーンを東京にします。
                 # Set-TimeZone -Id "Tokyo Standard Time"
 
-                Restart-Computer
+                # Ignore errors as it seems a shutdown is in progress anyway, possibly due to setting the System Locale.
+                # "Failed to restart the computer EC2AMAZ-1J2TNAU with the following error message: A system shutdown is in progress"
+                Restart-Computer -ErrorAction SilentlyContinue
             }
         }
     }
