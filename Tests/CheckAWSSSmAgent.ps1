994fd4074c4ce2187df02ae040032be4dca5effe
Rizwan Ansari <rizwan@celestialsys.com>
2021-07-09 19:19:58 +0530

    163405 Fix merge conflict

--- Tests/CheckAWSSSmAgent.ps1
+++ Tests/CheckAWSSSmAgent.ps1
@@ -1,24 +1,18 @@
-
 $Cloud = (Get-ItemProperty -Path HKLM:\Software\LANSA -Name 'Cloud').Cloud
 if ($Cloud -eq "AWS") {
-
-    #Check if service Amazon SSM Agent is running or not
+    Write-Host "Check if Amazon SSM Agent is running or not"
     Try {
-
         $SSMService = Get-Service -Name "Amazon SSM Agent" -ErrorAction SilentlyContinue
         if ( $SSMService.Status -eq "Running" ) {
             Write-Host "Amazon SSM Agent is running"
         } else {
-            Throw
+            throw
         }
     } Catch {
-
         cmd /c exit 1 #Set $LASTEXITCODE
         throw "Amazon SSM Agent is not running"
     }
 } elseif ( $Cloud -eq "" ) {
-
     cmd /c exit 1 #Set $LASTEXITCODE
-    Throw "Cloud registry entry is empty"
-    
+    throw "Cloud registry entry is empty"
 }
