d3301c9eac492a91582578cb8bfe105bcfdd1573
Rob Goodridge <rob.goodridge@lansa.com.au>
2021-07-14 20:15:46 +1000

    163405 Wait 400 seconds for SSM Agent to start

--- Tests/CheckAWSSSmAgent.ps1
+++ Tests/CheckAWSSSmAgent.ps1
@@ -1,14 +1,20 @@
 $Cloud = (Get-ItemProperty -Path HKLM:\Software\LANSA -Name 'Cloud').Cloud
 if ($Cloud -eq "AWS") {
     Write-Host "Check if Amazon SSM Agent is running or not"
-    Try {
+    $Count = 20
+    $timeout = 20
+    for ($i = 0; $i -lt $Count; $i++ ) {
         $SSMService = Get-Service -Name "Amazon SSM Agent" -ErrorAction SilentlyContinue
         if ( $SSMService.Status -eq "Running" ) {
             Write-Host "Amazon SSM Agent is running"
-        } else {
-            throw
+            break;
         }
-    } Catch {
+
+        Write-Host "Waiting for Amazon SSM Agent to start"
+        Start-Sleep $timeout
+    }
+
+    if ( $i -ge $count) {
         cmd /c exit 1 #Set $LASTEXITCODE
         throw "Amazon SSM Agent is not running"
     }
