b31314118e049be0aa0abad6e61deef49ee84f62
Ganesh Kumar <ganesh.k@celestialsys.com>
2021-07-23 11:22:52 +0530

    163405: test pester tests

--- scripts/invoke-pester-tests.ps1
+++ scripts/invoke-pester-tests.ps1
@@ -1,10 +1,11 @@
 
 # Invoke Pester Tests
-cd $($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\Tests
-$result = Invoke-Pester -Script '.\Image*' -OutputFormat  NUnitXml -OutputFile '$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\Test-Vm.xml' -PassThru
-$result | Out-Default | Write-Host
-if ($result.Result -eq "Failed") {
-    throw "Failed Tests Count: $($result.FailedCount)"
-} else {
-    Write-Host "Tested the image successfully."
-}
+cd "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)/Tests"
+ls
+#$result = Invoke-Pester -Script '.\Image*' -OutputFormat  NUnitXml -OutputFile '$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\Test-Vm.xml' -PassThru
+#$result | Out-Default | Write-Host
+#if ($result.Result -eq "Failed") {
+#    throw "Failed Tests Count: $($result.FailedCount)"
+#} else {
+#    Write-Host "Tested the image successfully."
+#}
