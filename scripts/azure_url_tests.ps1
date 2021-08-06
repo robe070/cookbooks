a99404bcb6e0e4c3432551cb0f598440a2bce6b5
Ganesh Kumar <ganesh.k@celestialsys.com>
2021-08-06 11:52:23 +0530

    163405 improve logging

--- scripts/azure_url_tests.ps1
+++ scripts/azure_url_tests.ps1
@@ -1,16 +1,29 @@
 param (
     [Parameter(Mandatory=$true)]
     [string]
-    $deploymentOutput
+    $deploymentOutput,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $Language
 )
 
+Write-Host "DeploymentOutput is $deploymentOutput"
+Write-Host "Language is $Language"
 # Use the deployment output to extract the IpAddress
 $sqlazureDeploymentOutput=ConvertFrom-Json $deploymentOutput
 $IpAddress = $sqlazureDeploymentOutput.lbFqdn.value
 $url1 = "$IpAddress/cgi-bin/probe"
 $url2 = "$IpAddress/cgi-bin/lansaweb?about"
-$url3 = "$IpAddress/cgi-bin/lansaweb?wam=DEPTABWA&webrtn=BuildFirst&ml=LANSA:XHTML&part=DEX&lang=ENG"
-$url4 = "$IpAddress/cgi-bin/lansaweb?wam=JSMLICE&webrtn=weblic&ml=LANSA:XHTML&part=DEX&lang=ENG"
+
+if($Language -eq 'JPN') {
+  $url3 = "$IpAddress/JPNTESTs/TEST"
+  $url4 = "$IpAddress/JPNTESTs/DUMMY"
+} else {
+  $url3 = "$IpAddress/cgi-bin/lansaweb?wam=DEPTABWA&webrtn=BuildFirst&ml=LANSA:XHTML&part=DEX&lang=ENG"
+  $url4 = "$IpAddress/cgi-bin/lansaweb?wam=JSMLICE&webrtn=weblic&ml=LANSA:XHTML&part=DEX&lang=ENG"
+}
+
 $urls = @($url1, $url2, $url3, $url4)
 add-type @"
     using System.Net;
@@ -26,20 +39,21 @@ add-type @"
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 $failureCount = 0
 forEach($url in $urls) {
+    Write-Host $url
     try{
         $response = Invoke-WebRequest -Uri $url -TimeoutSec 14
         $ResponseCode = $response.StatusCode
         if($ResponseCode -ne 200) {
-            Write-Host "Response code not equal to 200: $ResponseCode" | Out-Default | Write-Verbose
+            Write-Host "Response code not equal to 200: $ResponseCode"
             $failureCount = $failureCount + 1
         } else {
-            Write-Host $ResponseCode | Out-Default | Write-Verbose
+            Write-Host $ResponseCode
         }
     } catch {
-        Write-Host $_.Exception | Out-Default | Write-Verbose
+        Write-Host $_.Exception
         $ResponseCode = $_.Exception.Response.StatusCode.Value__
         $failureCount = $failureCount + 1
-        Write-Host $ResponseCode | Out-Default | Write-Verbose
+        Write-Host $ResponseCode
     }
 }
 if($failureCount) {
