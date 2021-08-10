1a06fc76fd8020b5a5a662f7e163b7d54bccdc35
Ganesh Kumar <ganesh.k@celestialsys.com>
2021-08-10 12:33:49 +0530

    163405 Image CI Pipeline does not pull the PR into the VM to build the image

--- scripts/installGit.ps1
+++ scripts/installGit.ps1
@@ -28,7 +28,7 @@ Write-Debug "GitRepoPath = $GitRepoPath" | Write-Host
 $Update = $false
 
 try {
-    # Git outputs almost all normal messages to stderr. powershell interprets that as an error and 
+    # Git outputs almost all normal messages to stderr. powershell interprets that as an error and
     # displays the error text. To stop that stderr is redirected to stdout on the git commands.
 
     Write-Debug "Path = $([Environment]::GetEnvironmentVariable('PATH', 'Machine'))" | Write-Host
@@ -55,7 +55,7 @@ try {
         Write-Debug "Path = $([Environment]::GetEnvironmentVariable('PATH', 'Machine'))" | Write-Host
         $Update = $true
     }
-    Write-Host "Git installed" 
+    Write-Host "Git installed"
 
     Write-Debug "Path = $([Environment]::GetEnvironmentVariable('PATH', 'Machine'))" | Write-Host
 
@@ -65,17 +65,35 @@ try {
     cmd /c git reset --hard HEAD '2>&1' | Out-Default | Write-Host
     # Ensure we have all changes
     cmd /c git fetch --all '2>&1' | Out-Default | Write-Host
-    # Check out a potentially different branch
-    Write-Host "Branch: $Branch"
-    cmd /c git checkout -f $Branch  '2>&1' | Out-Default | Write-Host
-    if ($LASTEXITCODE -ne 0 -and $LASTEXITCODE -ne 128) 
-    {
-        cmd /c exit $LastExitCode;
-        throw 'Git checkout failed'
+    if ($Branch -Match "refs/pull/") {
+      $BRANCHNAME = 'pr_branch'
+      $PRID = $Branch.split("/")[2]
+      Write-Host "fetching PR changes pull/$PRID/head:$BRANCHNAME"
+      cmd /c git fetch origin pull/$PRID/head:$BRANCHNAME '2>&1' | Out-Default | Write-Host
+      if ($LASTEXITCODE -ne 0 -and $LASTEXITCODE -ne 128)
+      {
+          cmd /c exit $LastExitCode;
+          throw 'Git checkout failed'
+      }
+      Write-Host "checkout to branch $BRANCHNAME"
+      cmd /c git checkout -f $BRANCHNAME  '2>&1' | Out-Default | Write-Host
+      if ($LASTEXITCODE -ne 0 -and $LASTEXITCODE -ne 128)
+      {
+          cmd /c exit $LastExitCode;
+          throw 'Git checkout failed'
+      }
+    } else {
+      # Check out a potentially different branch
+      Write-Host "Branch: $Branch"
+      cmd /c git checkout -f $Branch  '2>&1' | Out-Default | Write-Host
+      if ($LASTEXITCODE -ne 0 -and $LASTEXITCODE -ne 128)
+      {
+          cmd /c exit $LastExitCode;
+          throw 'Git checkout failed'
+      }
+      # Finally make sure the current branch matches the origin
+      cmd /c git pull '2>&1' | Out-Default | Write-Host
     }
-    # Finally make sure the current branch matches the origin
-    cmd /c git pull '2>&1' | Out-Default | Write-Host
-
     Write-Debug "Path = $([Environment]::GetEnvironmentVariable('PATH', 'Machine'))" | Write-Host
 } catch {
     $_
