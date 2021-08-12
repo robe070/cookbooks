593a5849f3038b0ce51d5e3dec48aa1ec4e8a3c1
Rob Goodridge <rob.goodridge@lansa.com.au>
2021-08-12 16:53:51 +1000

    163405 Perform minimal JPN configuration because we know in Azure it will be overwritten anyway. This also allows this script to be executed after specialization to fix up Azure putting the VM back to English

--- scripts/language-pack-config-2.ps1
+++ scripts/language-pack-config-2.ps1
@@ -15,11 +15,11 @@ switch ( $Platform) {
         switch ( $Language ) {
             "jpn" {
                 $LangCode = 'ja-JP'
-                $Timezone = "Tokyo Standard Time"
+                # $Timezone = "Tokyo Standard Time"
 
-                Write-Host( "Set the location to Japan")
-                # Is this appropriate? This image may be started in any region of the world. What does it mean to be 'in Japan' when you may be running anywhere?
-                Set-WinHomeLocation -GeoId 0x7A
+                # Write-Host( "Set the location to Japan")
+                # # Is this appropriate? This image may be started in any region of the world. What does it mean to be 'in Japan' when you may be running anywhere?
+                # Set-WinHomeLocation -GeoId 0x7A
             }
         }
     }
@@ -27,11 +27,11 @@ switch ( $Platform) {
         switch ( $Language ) {
             "jpn" {
                 $LangCode = 'ja-JP'
-                $Timezone = "Tokyo Standard Time"
+                # $Timezone = "Tokyo Standard Time"
 
-                Write-Host( "Set the location to Japan")
-                # Is this appropriate? This image may be started in any region of the world. What does it mean to be 'in Japan' when you may be running anywhere?
-                Set-WinHomeLocation -GeoId 0x7A
+                # Write-Host( "Set the location to Japan")
+                # # Is this appropriate? This image may be started in any region of the world. What does it mean to be 'in Japan' when you may be running anywhere?
+                # Set-WinHomeLocation -GeoId 0x7A
             }
         }
     }
@@ -46,24 +46,26 @@ Set-WinUILanguageOverride -Language $LangCode
 Write-Host( "Set the system locale to Japan")
 Set-WinSystemLocale -SystemLocale $LangCode
 
-Write-Host( "Set timezone to Tokyo time")
-Set-TimeZone -Id $Timezone
-
-Write-Host( "Make sysprep set the language & timezone correctly")
-
-$filename = "$ENV:ProgramData\Amazon\EC2-Windows\Launch\Sysprep\Unattend.xml"
-if ( Test-Path $filename) {
-    Write-Host( "Presuming AWS image")
-    $Doc = Get-Content $filename
-    $Doc | % { $_.Replace("en-US", $LangCode) } | % { $_.Replace("UTC", $Timezone) } | Set-Content $filename
-} else {
-    Write-Host( "Presuming Azure image")
-    $filename = "c:\lansa\scripts\AzureLanguageUnattend.xml"
-    mkdir "c:\lansa\sysprep" -ErrorAction SilentlyContinue
-    $Target = "c:\lansa\sysprep\Unattend.xml"
-    $Doc = Get-Content $filename
-    $Doc | % { $_.Replace("en-US", $LangCode) } | % { $_.Replace("UTC", $Timezone) } | Set-Content $Target
+if ( $Timezone ) {
+    Write-Host( "Set timezone to Tokyo time")
+    Set-TimeZone -Id $Timezone
 }
+
+# Write-Host( "Make sysprep set the language & timezone correctly")
+
+# $filename = "$ENV:ProgramData\Amazon\EC2-Windows\Launch\Sysprep\Unattend.xml"
+# if ( Test-Path $filename) {
+#     Write-Host( "Presuming AWS image")
+#     $Doc = Get-Content $filename
+#     $Doc | % { $_.Replace("en-US", $LangCode) } | % { $_.Replace("UTC", $Timezone) } | Set-Content $filename
+# } else {
+#     Write-Host( "Presuming Azure image")
+#     $filename = "c:\lansa\scripts\AzureLanguageUnattend.xml"
+#     mkdir "c:\lansa\sysprep" -ErrorAction SilentlyContinue
+#     $Target = "c:\lansa\sysprep\Unattend.xml"
+#     $Doc = Get-Content $filename
+#     $Doc | % { $_.Replace("en-US", $LangCode) } | % { $_.Replace("UTC", $Timezone) } | Set-Content $Target
+# }
 
 Start-Sleep -Seconds 30
 Restart-Computer -ErrorAction SilentlyContinue
