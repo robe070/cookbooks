e8c8863c8de26178daaaf2ace20574de74c67ac8
Rob Goodridge <rob.goodridge@lansa.com.au>
2021-08-20 15:12:21 +1000

    163405 Script to list all running stacks in all regions for the purpose of deleting them

---
+++ CloudFormationWindows/ListAllStacks.ps1
@@ -0,0 +1,10 @@
+
+$Regions = Get-AWSRegion
+foreach ($Region in $Regions) {
+    Write-Host( $Region )
+    if ( $Region -like 'us-iso*') {
+        Write-Host( "Skipping $Region")
+        Continue
+    }
+    Get-CFNStackSummary -Region $Region -StackStatusFilter @("CREATE_COMPLETE", "CREATE_FAILED", "DELETE_FAILED") | Where-Object {$_.ParentId -eq $null} | select-Object -Property StackName, StackStatus | Format-Table
+}
