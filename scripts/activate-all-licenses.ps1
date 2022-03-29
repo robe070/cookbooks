d5d2db2eda1a0c3c0869edadf6856e30a7f9d318
Rob Goodridge <rob.goodridge@lansa.com.au>
2022-03-29 11:13:20 +1030

    163681 Pester Tests - check for Cloud Account Id licenses and throw a fatal error if there are neither scalable licenses nor cloud licenses

--- scripts/activate-all-licenses.ps1
+++ scripts/activate-all-licenses.ps1
@@ -30,6 +30,8 @@ try
 {
 	Write-output ("Remap licenses to new instance Guid and set permissions so that $webuser may access them" )
 
+    $LicenseCount = 0
+
     [string[][]]$Keys = @(@("LANSA Scalable License", "ScalableLicensePrivateKey"), @("LANSA Integrator License", "IntegratorLicensePrivateKey"), @("LANSA Development License", "DevelopmentLicensePrivateKey") )
     foreach ( $LicensePrivateKey in $Keys ) {
         $LicensePrivateKey[0]
@@ -37,9 +39,31 @@ try
         $LicensePrivateKeyValue = Get-ItemProperty -Path HKLM:\Software\LANSA  -Name $LicensePrivateKey[1] -ErrorAction SilentlyContinue
         if ( $LicensePrivateKeyValue ) {
             Map-LicenseToUser $LicensePrivateKey[0] $LicensePrivateKey[1] $webuser
+            $LicenseCount++
         } else {
             Write-Warning "$(LOG-DATE) $($LicensePrivateKey[1]) not installed"
         }
+    }
+
+    if ( $LicenseCount -eq 0 ) {
+        # This code will throw an error at some stage unless a Cloud Account Id license file exists
+        Write-Host( "No scalable licenses found. Look for Cloud Account Id licenses..." )
+        $LicenseDir = (Get-ItemProperty -Path HKLM:\Software\lansa\Common -Name 'LicenseDir' -ErrorAction Continue).LicenseDir
+        if ( -not $LicenseDir ) {
+            throw "The LicenseDir registry entry does not exist"
+        }
+
+        if ( Test-Path $LicenseDir ) {
+            if ( Test-Path "$LicenseDir\x_lic*.5.lic" ) {
+                Write-Host "List the Cloud Account Id licenses..."
+                Get-ChildItem "$LicenseDir\x_lic*.5.lic"
+            } else {
+                throw "There are no cloud account id licenses"
+            }
+        } else {
+            throw "The Cloud Account Id license directory $LicenseDir does not exist"
+        }
+        Write-Host( "Cloud Account Id license(s) exist")
     }
 
     Write-Output ("License activation completed successfully")
