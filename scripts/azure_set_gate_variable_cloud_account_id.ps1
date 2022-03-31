0ba0fa458e7765054edfcbf397461a3b2135cc06
Rob Goodridge <rob.goodridge@lansa.com.au>
2022-03-31 13:23:54 +1030

    163681 Provide support for Azure Cloud Account Id DevOps testing

---
+++ scripts/azure_set_gate_variable_cloud_account_id.ps1
@@ -0,0 +1,45 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Version,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $osName
+   )
+
+Write-Host "version is - $Version"
+# Set the Gate variable if the file exists
+$path = "$($env:System_DefaultWorkingDirectory)/_Build Cloud Account Id Artefacts/$Version/$Version.txt"
+if (Test-Path $path) {
+    # Remove characters from Version to reduce length to less than 9 and which are not compatible with resource ids in the template.
+    # In particular, the VM base name in a Scale Set and
+    # $VersionClean = $Version -replace '[-]',''
+    # Randomize the Version because its being used as an ID that is causing duplicates if just use the version number.
+    $VersionClean = ""
+    1..7 | ForEach {
+        $code = Get-Random -Minimum 65 -Maximum 90 # Upper case letters only
+        $VersionClean = $VersionClean + [char]$code
+    }
+    Write-Host "Clean version = $VersionClean"
+
+    $rawUri = Get-Content -Path $path -Raw
+    Write-Host "ImageUrl is $rawUri"
+    $rawUri -match '[\w-]+\.vhd'
+    Write-Host "ImageName value is $Matches[0]"
+    $Matches[0] -match '[^.]+'
+    $sku = $Matches[0]
+    Write-Host "SKU is $sku"
+    Write-Host "##vso[task.setvariable variable=Sku;isOutput=true]$sku"
+    $uri = "/subscriptions/$env:SUBSCRIPTIONID/resourceGroups/$env:RESOURCEGROUPNAME/providers/Microsoft.Compute/images/$($Matches[0])image"
+    # Set Variables
+    Write-Host "##vso[task.setvariable variable=ImageUrl;isOutput=true]$uri"
+    Write-Host "##vso[task.setvariable variable=IsEnabled;isOutput=true]True"
+    Write-Host "##vso[task.setvariable variable=osName;isOutput=true]$osName"
+    Write-Host "##vso[task.setvariable variable=Version;isOutput=true]$Version"
+    Write-Host "##vso[task.setvariable variable=VersionClean;isOutput=true]$VersionClean"
+    Write-Host "##vso[task.setvariable variable=Sku;isOutput=true]$sku"
+    Write-host "The value of Variable IsEnabled is updated to True and output variable ImageUrl to $uri"
+} else {
+    Write-Host "Artifact path $path does NOT exist for $Version"
+}
