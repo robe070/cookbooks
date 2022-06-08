4ec23c7999a2ecda478f5cd12114fb3aa7749571
Rob Goodridge <rob.goodridge@lansa.com.au>
2022-06-08 14:13:49 +0100

    163728 Make code consistent so that error messages are trapped

--- scripts/install-base-sql-server.ps1
+++ scripts/install-base-sql-server.ps1
@@ -36,7 +36,7 @@ try {
 
     #####################################################################################
     Write-Output ("$(Log-Date) Fix Windows Server 2019 error 0x800f0950 – Enable .NET 3.5 (error code -2146498224). First occurred 8th June 2022 on AWS. Required for IIS-NetFxExtensibility") | Out-File $OutputFile -Append
-    dism /online /enable-feature /featurename:NetFX3 | Out-File $OutputFile -Append
+    Run-ExitCode "C:\Windows\system32\dism.exe" @('/online', '/enable-feature', '/featurename:NetFX3', '/norestart', '/All')  | Out-File $OutputFile -Append
 
     Write-Output ("$(Log-Date) Installing IIS-NetFxExtensibility") | Out-File $OutputFile -Append
     Run-ExitCode "C:\Windows\system32\dism.exe" @('/online', '/enable-feature', '/featurename:IIS-NetFxExtensibility', '/norestart', '/All')  | Out-File $OutputFile -Append
