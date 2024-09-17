b24ad780b094918638f845a0e80741475fbed193
debjitbhowmik807 <debjit.bhowmik@accolitedigital.com>
2024-09-17 17:52:11 +0530

    update .net 4.8

--- scripts/bake-ide-ami.ps1
+++ scripts/bake-ide-ami.ps1
@@ -192,7 +192,100 @@ else
 {
 	throw '$IncludeDir must be set up by the caller'
 }
+# Check if the .NET Framework is installed
 
+if
+(!(Get-ItemProperty HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full)) {
+
+    Write-Output
+".NET Framework is not installed"
+
+}
+
+# Check the current version of the .NET Framework
+
+$dotNetVersion
+= (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full).Version
+
+Write-Output
+"Current .NET Framework version:
+$dotNetVersion
+"
+
+# Check if an update is available
+
+$updateSession
+= New-Object -ComObject Microsoft.Update.Session
+
+$updateSearcher
+=
+$updateSession
+.CreateUpdateSearcher()
+
+$searchResult
+=
+$updateSearcher
+.Search(
+"Type='Software' and IsInstalled=0 and DeploymentAction='Installation' and Title='Microsoft .NET Framework 4.8'"
+).Updates
+
+if
+(
+$searchResult
+.Count -eq 0) {
+
+    Write-Output
+".NET Framework is up to date"
+
+}
+else
+{
+
+    Write-Output
+"Updating .NET Framework to version 4.8"
+
+   
+# Install the update
+
+   
+$updateInstaller
+=
+$updateSession
+.CreateUpdateInstaller()
+
+   
+$updateInstaller
+.Updates =
+$searchResult
+
+   
+$installationResult
+=
+$updateInstaller
+.Install()
+
+   
+# Check the result of the installation
+
+   
+if
+(
+$installationResult
+.ResultCode -eq 2) {
+
+        Write-Output
+".NET Framework update successful"
+
+    }
+else
+{
+
+        Write-Output
+"Error while updating .NET Framework"
+
+    }
+
+}
 ###############################################################################
 # Main program logic
 ###############################################################################
