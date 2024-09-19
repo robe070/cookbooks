684ac295e3108e9aa9cab709e222df6f4024c390
Divakshi <divakshi.singh@accolitedigital.com>
2024-09-19 10:58:10 +0530

    #184-changes in bake-ide-ami.ps1 for win22

--- scripts/bake-ide-ami.ps1
+++ scripts/bake-ide-ami.ps1
@@ -1041,9 +1041,11 @@ $jsonObject = @"
                         Remove-Item $filename | Out-Default | Write-Host;
                     }
                 }
-                Invoke-Command -Session $Script:session {cd $ENV:ProgramData\Amazon\EC2-Windows\Launch\Scripts | Out-Default | Write-Host}
-                Invoke-Command -Session $Script:session {./InitializeInstance.ps1 -Schedule | Out-Default | Write-Host}
-                Invoke-Command -Session $Script:session {./SysprepInstance.ps1 | Out-Default | Write-Host}
+                Invoke-Command -Session $Script:session {cd $ENV:ProgramFiles\Amazon\EC2Launch | Out-Default | Write-Host}
+                Invoke-Command -Session $Script:session {./ec2launch.exe sysprep -c -s | Out-Default | Write-Host}
+                # Invoke-Command -Session $Script:session {cd $ENV:ProgramData\Amazon\EC2-Windows\Launch\Scripts | Out-Default | Write-Host}
+                # Invoke-Command -Session $Script:session {./InitializeInstance.ps1 -Schedule | Out-Default | Write-Host}
+                # Invoke-Command -Session $Script:session {./SysprepInstance.ps1 | Out-Default | Write-Host}
             }
         } elseif ($Cloud -eq 'Azure' ) {
             Write-Host( "$(Log-Date) Running sysprep automatically")
