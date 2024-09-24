59b6c1e9ddc10f9d81de749238628696c7f959ed
debjitbhowmik807 <debjit.bhowmik@accolitedigital.com>
2024-09-24 10:43:56 +0530

    WIP

--- scripts/bake-ide-ami.ps1
+++ scripts/bake-ide-ami.ps1
@@ -683,33 +683,33 @@ $jsonObject = @"
             # Install Chocolatey
             Execute-RemoteScript -Session $Script:session -FilePath "$script:IncludeDir\getchoco.ps1"
 
-            if ( $Cloud -eq 'Azure' ) {
-                Write-Host("$(Log-Date) Azure requires a reboot after installing choco. Rebooting now..")
+            # if ( $Cloud -eq 'Azure' ) {
+            #     Write-Host("$(Log-Date) Azure requires a reboot after installing choco. Rebooting now..")
                 
-                Execute-RemoteBlock $Script:session {
-                    Restart-Computer -force
-                }
-                Start-Sleep -Seconds 10
+            #     Execute-RemoteBlock $Script:session {
+            #         Restart-Computer -force
+            #     }
+            #     Start-Sleep -Seconds 10
             
-                Write-Host "$(Log-Date) Reconnecting session..."
-                if ( $Script:session ) { Remove-PSSession $Script:session | Out-Default | Write-Host }
+            #     Write-Host "$(Log-Date) Reconnecting session..."
+            #     if ( $Script:session ) { Remove-PSSession $Script:session | Out-Default | Write-Host }
             
-                Connect-RemoteSession | Out-Default | Write-Host
-                Execute-RemoteInit | Out-Default | Write-Host
-                Execute-RemoteScript -Session $Script:session -FilePath "$script:IncludeDir\dot-CommonTools.ps1"
-                Write-host "$(Log-Date) Rebooted the VM!"
-                Execute-RemoteBlock $Script:session {
+            #     Connect-RemoteSession | Out-Default | Write-Host
+            #     Execute-RemoteInit | Out-Default | Write-Host
+            #     Execute-RemoteScript -Session $Script:session -FilePath "$script:IncludeDir\dot-CommonTools.ps1"
+            #     Write-host "$(Log-Date) Rebooted the VM!"
+            #     Execute-RemoteBlock $Script:session {
 
-                    Write-Host( "$(Log-Date) Path before changing it: $ENV:Path ")
+            #         Write-Host( "$(Log-Date) Path before changing it: $ENV:Path ")
   
-                   Add-DirectoryToEnvPathOnce -Directory "C:\ProgramData\chocolatey\bin\" | Out-Default | Write-Host
+            #        Add-DirectoryToEnvPathOnce -Directory "C:\ProgramData\chocolatey\bin\" | Out-Default | Write-Host
   
-                    Write-Host( "$(Log-Date) Path after changing it: $ENV:Path")
+            #         Write-Host( "$(Log-Date) Path after changing it: $ENV:Path")
   
-                   choco | Out-Default | Write-Host
+            #        choco | Out-Default | Write-Host
   
-                 }
-            }
+            #      }
+            # }
             
 
             # Then we install git using chocolatey and pull down the rest of the files from git
