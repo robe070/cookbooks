15f58663bea3b3cab61f2a96ead5d2157eea2300
priyadishah <priyanka.shah@accolitedigital.com>
2024-09-27 14:52:06 +0530

    update files/folders

--- scripts/install-lansa-base.ps1
+++ scripts/install-lansa-base.ps1
@@ -98,11 +98,6 @@ try
     if ( !(test-path $TempPath) ) {
         New-Item $TempPath -type directory -ErrorAction SilentlyContinue | Out-Default | Write-Host
     }
-    #  Enabling TLS 1.2 security protocol to establsih a secure connection with the server when making web requests.
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    
-    Write-Host( "$(Log-Date) Installing Windows Feature WebServer")
-    Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
     $Cloud = (Get-ItemProperty -Path HKLM:\Software\LANSA  -Name 'Cloud').Cloud
     $InstallSQLServer = $false
@@ -173,8 +168,7 @@ try
                 (New-Object System.Net.WebClient).DownloadFile($CWASetup, $installer_file) | Out-Default | Write-Host
                 $downloaded = $true
             } catch {
-                $_
-                $TotalFailedDownloadAttempts += 1 
+                $TotalFailedDownloadAttempts += 1
                 New-ItemProperty -Path HKLM:\Software\LANSA  -Name 'TotalFailedDownloadAttempts' -Value ($TotalFailedDownloadAttempts) -PropertyType DWORD -Force | Out-Null
                 $loops += 1
 
