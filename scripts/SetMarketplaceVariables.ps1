5117e9dd4db61177928685166f1aea20d9fe4163
Rob Goodridge <goodridge.rob@gmail.com>
2025-08-15 16:24:40 +1000

    164362 Add new Marketplace variable derivation

--- scripts/SetMarketplaceVariables.ps1
+++ scripts/SetMarketplaceVariables.ps1
@@ -1,6 +1,6 @@
 # SetMarketplaceVariables.ps1
-# This script sets Azure DevOps variables based on input parameters baseImageName and templateType
-# It uses a predefined table to map keys to AWS Marketplace template data
+# This script sets Azure DevOps variables based on input parameters baseImageName, templateType, and VersionDigits
+# It uses a predefined table to map keys to AWS Marketplace template data, with deduplicated elements
 
 param (
     [Parameter(Mandatory=$true)]
@@ -9,7 +9,10 @@ param (
 
     [Parameter(Mandatory=$true)]
     [ValidateSet('master', 'stacktype')]
-    [string]$templateType
+    [string]$templateType,
+
+    [Parameter(Mandatory=$true)]
+    [string]$VersionDigits
 )
 
 # Function to derive key components from baseImageName
@@ -20,31 +23,28 @@ function Get-KeyComponents {
 
     $key1 = $BaseImageName.Split('-')[0]  # e.g., w19d
     $key2 = if ($BaseImageName -like '*j') { 'jpn' } else { 'eng' }
+    $versionBase = $BaseImageName.Split('-')[1]  # e.g., 15
+    $versionMinor = $BaseImageName.Split('-')[2].Replace('j', '')  # e.g., 0, removing 'j' if present
+    $versionPrefix = "$versionBase.$versionMinor"  # e.g., 15.0
 
-    return $key1, $key2
+    return $key1, $key2, $versionPrefix
 }
 
-# Define the lookup table
+# Define the lookup table with deduplicated elements and combined w19d_eng entry
 $templateData = @{
-    'w19d_eng_stacktype' = @{
-        TemplateUrl = 'https://awsmp-cft-992382380361-1708727387563.s3.us-east-1.amazonaws.com/f632327c-e7fc-45fc-a594-d70141e84988/prod-7c4xdvxkskdfs/a50e24ef-1e9d-43fd-aa11-e7a2149ef9d7/lansa-stack-type-win.cfn.template'
-        MPS3BucketName = 'awsmp-cft-992382380361-1708727387563'
-        MPS3BucketRegion = 'us-east-1'
-        MPS3KeyPrefix = 'f632327c-e7fc-45fc-a594-d70141e84988/'
-    }
-    'w19d_eng_master' = @{
-        TemplateUrl = 'https://awsmp-cft-992382380361-1708727387563.s3.us-east-1.amazonaws.com/4d884544-55ce-48c3-b9e0-0e624968ff19/prod-7c4xdvxkskdfs/ed7dce1e-63fb-4e8b-99b2-a9c77236cb88/lansa-master-win.cfn.template'
-        MPS3BucketName = 'awsmp-cft-992382380361-1708727387563'
-        MPS3BucketRegion = 'us-east-1'
-        MPS3KeyPrefix = '4d884544-55ce-48c3-b9e0-0e624968ff19/'
+    'w19d_eng' = @{
+        ProductId = 'prod-7c4xdvxkskdfs'
+        StackTypeTemplateFile = 'a50e24ef-1e9d-43fd-aa11-e7a2149ef9d7/lansa-stack-type-win.cfn.template'
+        MasterTemplateFile = 'ed7dce1e-63fb-4e8b-99b2-a9c77236cb88/lansa-master-win.cfn.template'
+        TemplateKeyPrefix = 'f632327c-e7fc-45fc-a594-d70141e84988/'  # Using stacktype prefix; adjust if master prefix differs
     }
     # Additional key combinations can be added here as needed
-    # e.g., 'w19d_jpn_stacktype', 'w19d_jpn_master', 'w22d_eng_stacktype', etc.
+    # e.g., 'w19d_jpn', 'w22d_eng', 'w25d_jpn', etc.
 }
 
 # Get key components
-$key1, $key2 = Get-KeyComponents -BaseImageName $baseImageName
-$key = "${key1}_${key2}_${templateType}"
+$key1, $key2, $versionPrefix = Get-KeyComponents -BaseImageName $baseImageName
+$key = "${key1}_${key2}"
 
 Write-Host "##vso[task.setvariable variable=UseMarketplaceVariables]False"
 
@@ -52,12 +52,24 @@ Write-Host "##vso[task.setvariable variable=UseMarketplaceVariables]False"
 if ($templateData.ContainsKey($key)) {
     $data = $templateData[$key]
 
+    $MPS3BucketName = 'awsmp-cft-992382380361-1708727387563'
+    $MPS3BucketRegion = 'us-east-1'
+
+    # Select the appropriate template file based on templateType
+    $TemplateFile = if ($templateType -eq 'stacktype') { $data.StackTypeTemplateFile } else { $data.MasterTemplateFile }
+
+    # Construct variables
+    $TemplateUrl = "https://$($MPS3BucketName).s3.$($MPS3BucketRegion).amazonaws.com/$($data.TemplateKeyPrefix)$TemplateFile"
+    $MPS3KeyPrefix = $data.TemplateKeyPrefix
+    $ImageId = "/aws/service/marketplace/$($data.ProductId)/$versionPrefix.$VersionDigits"
+
     # Set Azure DevOps variables
     Write-Host "##vso[task.setvariable variable=UseMarketplaceVariables]True"
-    Write-Host "##vso[task.setvariable variable=TemplateUrl]$($data.TemplateUrl)"
-    Write-Host "##vso[task.setvariable variable=MPS3BucketName]$($data.MPS3BucketName)"
-    Write-Host "##vso[task.setvariable variable=MPS3BucketRegion]$($data.MPS3BucketRegion)"
-    Write-Host "##vso[task.setvariable variable=MPS3KeyPrefix]$($data.MPS3KeyPrefix)"
+    Write-Host "##vso[task.setvariable variable=TemplateUrl]$TemplateUrl"
+    Write-Host "##vso[task.setvariable variable=MPS3BucketName]$MPS3BucketName"
+    Write-Host "##vso[task.setvariable variable=MPS3BucketRegion]$MPS3BucketRegion"
+    Write-Host "##vso[task.setvariable variable=MPS3KeyPrefix]$MPS3KeyPrefix"
+    Write-Host "##vso[task.setvariable variable=ImageId]$ImageId"
 } else {
     Write-Error "No data found for key: $key"
     exit 1
