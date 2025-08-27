c9961947c2aec30be7619be660de9964e26b714c
Rob Goodridge <goodridge.rob@gmail.com>
2025-08-27 14:12:32 +1000

    164362 debug code to try and use lansa artifact source for git install

--- scripts/installGit.ps1
+++ scripts/installGit.ps1
@@ -36,8 +36,9 @@ try {
     if ( $InstallGit -and (-not (Test-Path $GitRepoPath) ) )
     {
         Write-Host "Installing Git"
-        Run-ExitCode 'choco' @('install', 'git', '-y', '--no-progress', '--force' ) | Out-Default | Write-Host
-    
+        Run-ExitCode 'choco' @('install', 'git.install', '-s=lansa', '-y', '--no-progress', '--force' ) | Out-Default | Write-Host
+        Run-ExitCode 'choco' @('install', 'git', '-s=lansa', '-y', '--no-progress', '--force' ) | Out-Default | Write-Host
+
         refreshenv | Out-Default | Write-Host
 
         # Note, the Git install overwrites the current environment so need to modify path here
