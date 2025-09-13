cd753fd497c640389878c4424fe1a0703815549d
Rob Goodridge <goodridge.rob@gmail.com>
2025-09-13 13:59:53 +1000

    JIRA LANSA-263 Log the choco install delay

--- scripts/getchoco.ps1
+++ scripts/getchoco.ps1
@@ -193,8 +193,8 @@ try {
             if ($i -le 1) {
                 throw "Failed to download chocolatey.nupkg from '$url' after $TotalAttempts attempts. Error: `n $_"
             } else {
-                Write-Host "Attempt $($TotalAttempts - $i + 1) to download chocolatey.nupkg failed. Retrying..."
                 $delay = [math]::Min(120 * [math]::Pow(4, $TotalAttempts - $i), 3600) # Cap at 1 hour
+                Write-Host "Attempt $($TotalAttempts - $i + 1) to download chocolatey.nupkg failed. Delaying $delay seconds before retrying..."
                 Start-Sleep -Seconds $delay # Exponential backoff time (120, 240, 1920, 7680, 30720,... seconds)
             }
         }
