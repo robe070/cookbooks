64e90be9adb678bcec3a0c058c753948832f5339
Rob Goodridge <goodridge.rob@gmail.com>
2025-09-16 16:53:09 +1000

    JIRA LANSA-263 Restore the checkout and fix up the syntax error - still more code to put in here, but lets see if this works first

--- scripts/GitRepoWriteAccess.ps1
+++ scripts/GitRepoWriteAccess.ps1
@@ -27,14 +27,13 @@ param (
 # goto git repo
 cd "$($env:Pipeline_Workspace)/$($GitRepoPath)"
 
-# Comment out this code as we are using the checkout step to get the correct branch.
 # Doing the checkout here means we regress the scripts to a potentially older version in the target branch, whereas we need the latest of the source branch
-# git checkout to branch
-# git checkout $GitBranch
-# if (-not $?) {
-#   Write-Host("git checkout $GitBranch failed");
-#   exit 1
-# }
+Write-Host "git checkout branch $GitBranch"
+git checkout $GitBranch
+if (-not $?) {
+  Write-Host("git checkout $GitBranch failed");
+  exit 1
+}
 
 Write-Host "Configuring git email '$GitUserEmail'"
 git config --global user.email "$GitUserEmail"
