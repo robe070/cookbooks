40822ce95284d6761ad234131fa66f682286d5be
Rob Goodridge <goodridge.rob@gmail.com>
2026-04-23 14:51:09 +1000

    JIRA LANSA-267 Must create new gallery image definition to store g2 images

--- scripts/bake-ide-ami.ps1
+++ scripts/bake-ide-ami.ps1
@@ -1149,7 +1149,7 @@ $jsonObject = @"
 
         # Add image to Azure Compute Gallery
         $GalleryName = "LansaGallery"
-        $ImageDefinitionName = $ImageName -replace "-(\d+j?|\d+)image$", "" # Handles "w16d-16-0-19image" => "w16d-16-0" and "w16d-16-0j-19image" => "w16d-16-0j"
+        $ImageDefinitionName = ($ImageName -replace "-(\d+j?|\d+)image$", "") + "-g2" # Handles "w16d-16-0-19image" => "w16d-16-0" and "w16d-16-0j-19image" => "w16d-16-0j"
         $versionNumbers = $VersionText -split '-' | Select-Object -Last 3
         $versionNumbers = $versionNumbers -replace 'j', ''
         $galleryImageVersion = $versionNumbers -join '.' # Ensure version format like "16.0.19" for both "w22d-16-0-19" and "w16d-16-0j-19"
